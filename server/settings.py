12d8299e0bd3a429cba7ce6175cb3efc03da8af8
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-08-10 16:31:19 +0200

    Fix flake8 warnings.

--- server/settings.py
+++ server/settings.py
@@ -1,8 +1,5 @@
-from pathlib import Path
 from server.components import config, BASE_DIR
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
@@ -71,16 +68,21 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "\
+            django.contrib.auth.password_validation.\
+            UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.\
+            password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.\
+            password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.\
+            password_validation.NumericPasswordValidator",
     },
 ]
 
