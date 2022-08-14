5280847d09f1ba05694b7d7bc3bed0842aa69338
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-08-14 09:33:51 +0200

    fix bugs

--- server/cshr/models/__init__.py
+++ server/cshr/models/__init__.py
@@ -1,14 +1,11 @@
-# from .abstracts import *
-# from .office import *
-# from .skills import *
-# from .users import *
-# from .compensation import *
-# from .training_courses import *
-# from .vacations import *
-# from .hr_letters import *
-# from .company_properties import *
-# from .evaluations import *
-# from .requests import *
-
-# remove the comment whenever you make
-# a migrations and add the new model if it exists
+from .abstracts import *
+from .office import *
+from .skills import *
+from .users import *
+from .compensation import *
+from .training_courses import *
+from .vacations import *
+from .hr_letters import *
+from .company_properties import *
+from .evaluations import *
+from .requests import *
