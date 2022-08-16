93d51ca4ee2a5e342fa822326e7be6bbc908af30
Alia Saddik <82057274+aliasaddik@users.noreply.github.com>
2022-08-16 13:36:24 +0200

    balck

--- server/cshr/models/abstracts.py
+++ server/cshr/models/abstracts.py
@@ -5,7 +5,6 @@ from django.db import models
 class TimeStamp(models.Model):
     """Database model for created and updated timestamps"""
 
- 
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     modified_at = models.DateTimeField(auto_now=True)
 
