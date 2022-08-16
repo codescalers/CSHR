a498da67b9dc3eb54f5ff9e5dff490dd52df064a
Alia Saddik <82057274+aliasaddik@users.noreply.github.com>
2022-08-16 13:35:01 +0200

    test1

---
+++ server/cshr/tests/test_auth.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from server.cshr.models.users import User
+
+class AuthenticationTests(APITestCase):
+    
+    def test_register_user(self) -> User:
+        """
+        Ensure we can create a user account.
+        """
+        url = '/api/auth/signup/'
+        data = {
+             "first_name": "alianew",
+             "last_name": "saddikaaa",
+             "telegram_link": "string",
+             "email": "user25201@example.com",
+             "birthday": "2022-08-16",
+             "mobile_number": "9389384298230",
+             "password": "password",
+             "location": 1
+}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
