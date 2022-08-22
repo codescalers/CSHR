ddc448f127aa4bcbce685e5b2d5ff08fe9df6c28
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-08-22 11:44:53 +0200

    fix route

--- server/cshr/routs/users.py
+++ server/cshr/routs/users.py
@@ -10,10 +10,10 @@ urlpatterns = [
     }), name="user"),
     path("user/", UserAPIView.as_view({
         'post': 'post',
-    }), name="users_post"), 
+    }), name="users_post"),
     path("user/search/<str:search_input>", UserAPIView.as_view({
         'get': 'search_input',
-    }), name="users_post"),
+    }), name="users_search"),
     path("users/", UserAPIView.as_view({
         'get': 'get_all'
     }), name="users_get")
