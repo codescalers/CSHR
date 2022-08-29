930f6f1dd4b50293b7265342401ecd89a2e7d10e
0oM4R <omarksm09@gmail.com>
2022-08-29 13:27:54 +0200

    add BaseUserAdmin to user model

--- server/cshr/admin/admin.py
+++ server/cshr/admin/admin.py
@@ -1,3 +1,22 @@
 # from django.contrib import admin
+from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+
+
+def autoregister(*app_list: str) -> None:
+    """
+    register all database models in the admin dashboard.
+    """
+    for app in app_list:
+        for model_name, model in apps.get_app_config(app).models.items():
+            if "_" not in model_name:
+                admin.site.register(model, globals().get(model.__name__ + "Admin"))
+            else:
+                admin.site.register(model)
+
+
+admin.site.site_header = "CSHR Administration Settings"
+admin.site.site_title = "CSHR Administration Settings"
+autoregister("cshr")
