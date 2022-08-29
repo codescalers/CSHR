8d25c8c0291ed421fb36e8ce210590a5f9127294
0oM4R <omarksm09@gmail.com>
2022-08-29 19:14:37 +0200

    solve #77 , add city field and set them all unique together

--- server/cshr/models/office.py
+++ server/cshr/models/office.py
@@ -7,6 +7,10 @@ from server.cshr.models.abstracts import TimeStamp
 class Office(TimeStamp):
     name = models.CharField(max_length=45)
     country = models.CharField(max_length=45)
+    city = models.CharField(max_length=45)
 
     def __str__(self) -> str:
         return self.name
+
+    class Meta:
+        unique_together = ("name", "country", "city")
