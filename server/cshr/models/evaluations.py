9485348924bef9e1cde6c7e40a5596d427af2396
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-08-31 14:06:06 +0200

    Added UserEvaluation model.

--- server/cshr/models/evaluations.py
+++ server/cshr/models/evaluations.py
@@ -4,11 +4,36 @@ from server.cshr.models.abstracts import TimeStamp
 from server.cshr.models.users import User
 
 
+class EVALUATION_FORM_TYPE(models.TextChoices):
+    PEER_2_PEER_FORM = "Peer 2 Peer Form"
+    REVERSE_FORM = "Reverse Form"
+
+
+class EVALUATION_QUARTER(models.TextChoices):
+    JAN_MARCH = "1 : 3"
+    APR_JUN = "4 : 6"
+    JUL_SEP = "7 : 9"
+    OCT_DEC = "10 : 12"
+
+
 class Evaluations(TimeStamp):
     """Database model for evaluations"""
 
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    form = models.CharField(max_length=50, choices=EVALUATION_FORM_TYPE.choices)
+    quarter = models.CharField(max_length=150, choices=EVALUATION_FORM_TYPE.choices)
     link = models.CharField(max_length=150)
 
     def __str__(self) -> str:
-        return f"user id :{self.user} {self.link}"
+        return f"({self.quarter}) - {self.created_at.year}"
+
+
+class UserEvaluations(TimeStamp):
+    """Database model for user evaluations"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quarter = models.CharField(max_length=150, choices=EVALUATION_FORM_TYPE.choices)
+    link = models.CharField(max_length=150)
+    score = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.user.full_name
