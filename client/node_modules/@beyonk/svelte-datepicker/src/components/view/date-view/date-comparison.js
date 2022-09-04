143d8da3eed24f04150e0e05283041d72c94d98e
mhmdhelmy209@gmail.com <mhmdhelmy209@gmail.com>
2022-09-04 12:23:42 +0200

    fixing actions

--- client/node_modules/@beyonk/svelte-datepicker/src/components/view/date-view/date-comparison.js
+++
@@ -1,6 +0,0 @@
-export function isDateBetweenSelected (a, b, c) {
-  const start = a.startOf('day').toDate()
-  const stop = b.startOf('day').toDate()
-  const day = c.startOf('day').toDate()
-  return day > start && day < stop
-}
