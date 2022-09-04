9259140de70bfd175e188b76c2271eb6642380ad
mhmdhelmy209@gmail.com <mhmdhelmy209@gmail.com>
2022-09-04 11:52:23 +0200

    finished notifications page

--- client/src/pages/Notifications.svelte
+++ client/src/pages/Notifications.svelte
@@ -2,7 +2,9 @@
     import type { UserInterface } from "../types";
     export let user: UserInterface;
     import Sidebar2 from "../components/sidebar/Sidebar2.svelte";
-    
+   
+    let pageCount = 0;
+    let pageSize = 3;    
     let requests = [{
         "name" : "Marc",
         "period": 2,
@@ -18,7 +20,51 @@
         "date": "28 May, 2000",
         "time": "6.30",
         "classPill":"primary",
-    }];
+    },
+    {
+        "name" : "Joe",
+        "period": 3,
+        "role": "Software Engineer",
+        "date": "28 May, 2000",
+        "time": "6.30",
+        "classPill":"primary",
+    },
+    {
+        "name" : "Michael",
+        "period": 2,
+        "role": "Engineering Manager",
+        "date": "28 May, 2000",
+        "time": "6.33",
+        "classPill":"danger",
+    },
+    {
+        "name" : "Marwan",
+        "period": 3,
+        "role": "SRE",
+        "date": "28 May, 2000",
+        "time": "6.33",
+        "classPill":"primary",
+    },
+    {
+        "name" : "Joel",
+        "period": 3,
+        "role": "QA Engineer",
+        "date": "28 May, 2000",
+        "time": "6.33",
+        "classPill":"primary",
+    },
+    ];
+    function increment() {
+      if (totalNotifications - (pageCount * 3) - pageSize  > 0 ){
+          pageCount++;
+      }
+    }
+    function decrement() {
+      if (pageCount > 0 ){
+          pageCount--;
+      }
+    }
+    let totalNotifications = requests.length;
 </script>
   
   <Sidebar2 bind:user={user}>
@@ -27,14 +73,7 @@
 
       <div>
           <h4 class="child mx-5">All Notifications</h4>
-          <div class="sort">
-            <box-icon name='sort-up'></box-icon>
-            <label class="text-muted mb-0">Sort</label>
-            
-            <box-icon name='filter-alt' type='solid' ></box-icon>
-            <label class="text-muted mb-0 filter">Filter</label>
-            
-        </div>
+          
           
          <table class="table align-middle mb-0 mx-5">
                 <thead >
@@ -46,7 +85,7 @@
                   </tr>
                 </thead>
                 <tbody>
-                    {#each  requests as request }
+                    {#each  requests.slice(pageCount * 3, pageCount * 3 + 3) as request}
                   <tr>
                     <td>
                       <div class="d-flex align-items-center">
@@ -78,21 +117,16 @@
                 </tbody>
               </table>
               <div class="pagination">
-               <div class="pagination-labels"><label class="text-muted mb-0 ">Rows per page:</label></div>
                <div class="pagination-labels">
-                <select >
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </div>
-              <div class="pagination-labels"> <label class="text-muted mb-0">1-8 of 1240</label> </div>
-              <div class="d-flex flex-column">
-                <i class="fa fa-angle-up" style="font-size:30px"></i>
-              <i class="fa fa-angle-down" style="font-size:30px"></i>
+                <label class="text-muted mb-0 ">Rows per page:</label>
+              </div>
+                
+              <div class="pagination-labels"> 
+                <label class="text-muted mb-0">{pageCount+1} of {totalNotifications / pageSize}</label>
+                <button on:click={decrement}><i class="icon fa-solid fa-chevron-left"></i></button>
+                <button on:click={increment}><i class="icon fa-solid fa-chevron-right"></i></button>
               </div>
               </div>
-             
               
           </div>        
     </section>
@@ -128,9 +162,11 @@ label {
   font-size: larger;
   padding-right: 0.5cm;
 }
-
+section {
+  overflow: hidden;
+}
 .icon {
-  padding-right: 0.2cm;
+  padding-right: 0.3cm;
 }
 
 </style>
