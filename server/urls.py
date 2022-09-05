37afa8495f1bc84a18c40da887b16687fcb41ab1
andrew-nassef <andrew.nassef@ayhoo.com>
2022-09-05 11:29:32 +0200

    add service , serializer , routs ,tests for COMPENSATION

--- server/urls.py
+++ server/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
                 path("meeting/", include("server.cshr.routs.meetings")),
                 path("event/", include("server.cshr.routs.event")),
                 path("home/", include("server.cshr.routs.landing_page")),
+                path("compensation/", include("server.cshr.routs.compensation")),
             ]
         ),
     ),
