ef1de512d82562e061f2f1c1220fb2eeae4ea51d
mhmdhelmy209@gmail.com <mhmdhelmy209@gmail.com>
2022-09-07 11:00:19 +0200

    added celery tasks after refactoring

---
+++ server/cshr/celery/__init__.py
@@ -0,0 +1,2 @@
+from .send_email import app as celery_app
+__all__ = ('celery_app',)
--- server/cshr/celery/send_email.py
+++ server/cshr/celery/send_email.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from celery import Celery
 from server.components import config
 from celery.schedules import crontab
+import datetime
+from django.core.mail import send_mail
 
 app = Celery("tasks", broker=config("REDIS_HOST"))
 
@@ -19,17 +21,20 @@ mail_title = "Probation period update"
 @app.task(name="send_email")
 def send_email():
     from server.cshr.models.users import USER_TYPE
-    import datetime
-    from django.core.mail import send_mail
+    
     from server.cshr.models.users import User
 
     date_since_month_and_a_half = datetime.datetime.now() - datetime.timedelta(45)
     date_since_three_months = datetime.datetime.now() - datetime.timedelta(90)
     users_joined_month_and_a_half_ago = User.objects.filter(
-        created_at__contains=date_since_month_and_a_half
+        created_at__year=date_since_month_and_a_half.year, 
+        created_at__month=date_since_month_and_a_half.month,
+        created_at__day=date_since_month_and_a_half.day,
     )
     users_joined_three_months_ago = User.objects.filter(
-        created_at__contains=date_since_three_months
+        created_at__year=date_since_three_months.year, 
+        created_at__month=date_since_three_months.month,
+        created_at__day=date_since_three_months.day,
     )
     admins = User.objects.filter(user_type=USER_TYPE.ADMIN)
     admins_emails = []
@@ -50,23 +55,23 @@ def send_email():
             fname=user.first_name, lname=user.last_name
         )
         send_mail(
-            user_msg,
             mail_title,
+            user_msg,
             settings.EMAIL_HOST_USER,
             [
                 user_email,
             ],
         )
         send_mail(
-            msg,
             mail_title,
+            msg,
             settings.EMAIL_HOST_USER,
             [
                 user_email,
                 supervisor_email,
             ],
         )
-        send_mail(msg, mail_title, settings.EMAIL_HOST_USER, admins_emails)
+        send_mail(mail_title, msg, settings.EMAIL_HOST_USER, admins_emails)
 
     for user in users_joined_three_months_ago:
         user_email = user.email
@@ -81,19 +86,19 @@ def send_email():
             fname=user.first_name, lname=user.last_name
         )
         send_mail(
-            user_msg,
             mail_title,
+            user_msg,
             settings.EMAIL_HOST_USER,
             [
                 user_email,
             ],
         )
         send_mail(
-            msg,
             mail_title,
+            msg,
             settings.EMAIL_HOST_USER,
             [
                 supervisor_email,
             ],
         )
-        send_mail(msg, mail_title, settings.EMAIL_HOST_USER, admins_emails)
+        send_mail(mail_title, msg,settings.EMAIL_HOST_USER, admins_emails)
