b5d64cadbd858689cc62aa8c90caf102047cd5d4
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-09-08 02:44:52 +0200

    people input

--- client/src/components/select/PeopleSelect.svelte
+++ client/src/components/select/PeopleSelect.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
+  import userDataService from "../../services/axios/users/UserDataService";
   import { onMount } from "svelte";
   import PeopleSlot from "./PeopleSlot.svelte";
-  let options: any[] = [];
-
   import MultiSelect from "svelte-multiselect";
-  import PeopleSlot from "./PeopleSlot.svelte";
-
-  export let selected = [];
-  export let placeholder = `Select languages`;
-  export let removeAllTitle = "Remove all";
+  export let selected: any[] = [];
+  export let maxSelect: number | null = null;
+  export let placeholder = `Select Users`;
+  export let removeAllTitle = "Remove all users";
+  let options: any[] = [];
+  onMount(async () => {
+    let { data } = await userDataService.getAll();
+    options = data.map((user: any) => {
+      return {
+        id: user.id,
+        full_name: user.full_name,
+        image: user.image,
+      };
+    });
+  });
 </script>
 
 <code>selected = {JSON.stringify(selected)}</code>
 <MultiSelect
   {options}
-  maxSelect={5}
+  {maxSelect}
   {placeholder}
+  bind:selected
   outerDivClass="foo"
   ulSelectedClass="bar"
   ulOptionsClass="baz"
@@ -25,16 +35,15 @@
   liActiveOptionClass="mom"
   sortSelected={true}
   autocomplete="on"
-  --sms-border-radius="5px"
+  --sms-border-radius="0.3rem"
   --sms-bg="#edf2f9"
   --sms-options-bg="white"
-  --sms-min-height="20px"
-  --sms-min-width="20px"
+  --sms-min-height="3rem"
+  --sms-min-width="4rem"
 >
   <PeopleSlot let:option {option} slot="selected" />
   <PeopleSlot let:option {option} slot="option" />
 </MultiSelect>
 
-<MultiSelect bind:selected {options} {placeholder} {removeAllTitle} />
 <!--   https://www.npmjs.com/package/svelte-multiselect -->
 <!-- https://svelte-multiselect.netlify.app/#with-css-variables -->
--- client/src/components/select/PeopleSlot.svelte
+++ client/src/components/select/PeopleSlot.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-  export let fullName: string;
-  export let imgLink: string;
+  export let option;
+  const { full_name, image } = option;
 </script>
 
 <div class="d-flex flex-column gap-2">
-  <img src={process.env.APP_BASE_API_URL + imgLink} /><span>{fullName}</span>
+  <img src={image} alt={full_name + " avatar"} /><span>{full_name}</span>
 </div>
 
 <style>
   img {
-    width: 100px;
-    height: 100px;
+    width: 1.5rem;
+    height: 1.6rem;
     border-radius: 50%;
+    object-fit: contain;
   }
   span {
-    font-size: 1.5rem;
-    font-weight: 600;
+    font-size: 1rem;
+    font-weight: 400;
     text-align: right;
   }
 </style>
