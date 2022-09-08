23410932835d82e5afe63b907f4f38f7e00d6d41
andrew-nassef <andrew.nassef@ayhoo.com>
2022-09-08 10:36:38 +0200

    add request endpoint with testing

--- server/cshr/serializers/hr_letters.py
+++ server/cshr/serializers/hr_letters.py
@@ -1,5 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    SerializerMethodField,
+)
 from server.cshr.models.hr_letters import HrLetters
+from server.cshr.serializers.users import BaseUserSerializer
 
 
 class HrLetterSerializer(ModelSerializer):
@@ -13,3 +17,31 @@ class HrLetterUpdateSerializer(ModelSerializer):
     class Meta:
         model = HrLetters
         exclude = ("approval_user",)
+
+
+class LandingPageHrLetterSerializer(ModelSerializer):
+    """Implemented to return just custom Hr Letters fields to landing page endpoint."""
+
+    applying_user = SerializerMethodField()
+    approval_user = SerializerMethodField()
+
+    class Meta:
+        model = HrLetters
+        fields = [
+            "addresses",
+            "status",
+            "applying_user",
+            "approval_user",
+        ]
+
+    def get_applying_user(self, obj: HrLetters) -> BaseUserSerializer:
+        """
+        this function return request's applying user
+        """
+        return BaseUserSerializer(obj.applying_user).data
+
+    def get_approval_user(self, obj: HrLetters) -> BaseUserSerializer:
+        """
+        this function return request's approval user
+        """
+        return BaseUserSerializer(obj.approval_user).data
