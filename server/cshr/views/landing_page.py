618c556838c2aff7244df7dd35d114d09b5d3144
mhmdhelmy209@gmail.com <mhmdhelmy209@gmail.com>
2022-09-11 12:28:12 +0200

    fixing actions

--- server/cshr/views/landing_page.py
+++ server/cshr/views/landing_page.py
@@ -3,7 +3,6 @@ from rest_framework.generics import GenericAPIView
 from server.cshr.api.permission import UserIsAuthenticated
 from server.cshr.api.response import CustomResponse
 from typing import Any
-from server.cshr.utils.script import run
 from server.cshr.services.landing_page import landinf_page_caliender_functionalty
 
 
@@ -11,7 +10,6 @@ class LandingPageApiView(GenericAPIView):
     permission_classes = (UserIsAuthenticated,)
 
     def get(self, request):
-        run()
         month: str = request.query_params.get("month")
         year: str = request.query_params.get("year")
         events: Any = landinf_page_caliender_functionalty(month, year)
