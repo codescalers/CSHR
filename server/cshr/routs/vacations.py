931cc978464943186fc36fe049690d857b91c773
zeina elkassas <zeinaahmed927@gmail.com>
2022-09-18 15:01:02 +0200

    black fixes

--- server/cshr/routs/vacations.py
+++ server/cshr/routs/vacations.py
@@ -1,21 +1,21 @@
 from django.urls import path
 from server.cshr.views.vacations import (
-    VacationApprovalAPIView,
-    VacationCommentsAPIView,
-    VacationsApiView,
-    VacationsUpdateApiView,
- )
+     VacationApprovalAPIView,
+     VacationCommentsAPIView,
+     VacationsApiView,
+     VacationsUpdateApiView,
+)
+
 urlpatterns = [
     path("", VacationsApiView.as_view({"get": "get_all", "post": "post"})),
     path("<str:id>/", VacationsApiView.as_view({"get": "get_one", "delete": "delete"})),
     path(
-         "edit/<str:id>/",
-         VacationsUpdateApiView.as_view(
-             {
-                 "put": "put",
-             }
-         ),
-
+       "edit/<str:id>/",
+       VacationsUpdateApiView.as_view(
+           {
+               "put": "put",
+           }
+       ),
     ),
     path("put/<str:id>/", VacationApprovalAPIView.as_view()),
     path("comment/<str:id>/", VacationCommentsAPIView.as_view()),
