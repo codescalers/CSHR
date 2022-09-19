35d811d1f09cafd95c7529ee62a8770efc7c9824
zeina elkassas <zeinaahmed927@gmail.com>
2022-09-19 13:59:01 +0200

    fixes

--- server/cshr/tests/test_vacation.py
+++ server/cshr/tests/test_vacation.py
@@ -264,7 +264,7 @@ class VacationsTests(APITestCase):
         response = client.put(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-def test_update_vacation_change_log(self) -> Vacation:
+    def test_update_vacation_change_log(self) -> Vacation:
         """update status and add a comment"""
         url = "/api/vacations/"
         data = {
@@ -320,7 +320,8 @@ def test_update_vacation_change_log(self) -> Vacation:
         data = {"status": "Approved"}
         response = client.put(url, data, format="json")
         self.assertEqual(response.status_code, 403)
-        def test_get_vacations_for_user_with_data(self) -> Vacation:
+
+    def test_get_vacations_for_user_with_data(self) -> Vacation:
         """add vacation request"""
         url = "/api/vacations/"
         data = {
@@ -345,4 +346,3 @@ def test_update_vacation_change_log(self) -> Vacation:
         url = "/api/vacations/user/"
         response = client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-     
