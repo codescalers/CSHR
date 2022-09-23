9c1570c9d7ddaf1103507c16371c34ba19290d4c
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-09-23 20:47:57 +0200

    update cookie

--- client/src/services/cookie/Cookie.ts
+++ client/src/services/cookie/Cookie.ts
@@ -3,23 +3,55 @@ class Cookie {
 	constructor() {
 		this.storage = document.cookie;
 	}
+	// to check if cookie exists
+	isFound(key: string) {
+		return this.storage.indexOf(key) !== -1;
+	}
+	// get cookie value by key name
 	get(key: string) {
-		return this.storage.split(";").filter((item) => {
-			return item === key;
-		})[0].split("=")[1];
-
+		if (this.isFound(key)) {
+			return this.storage.split(";").filter((item) => {
+				return item.indexOf(key) !== -1;
+			}).join(";");
+		}
+		else {
+			throw new Error("Cookie not found to be retrieved");
+		}
 	}
-
+	// to set cookie value
 	set(key: string, value: string, expiresMonth: number) {
-		const date = new Date();
-		date.setMonth(date.getMonth() + expiresMonth);
-		document.cookie = key + "=" + value + ";expires=" + date.toUTCString();
+		if (!this.isFound(key)) {
+			const date = new Date();
+			date.setMonth(date.getMonth() + expiresMonth);
+			document.cookie = `${key}=${value};expires=${date.toUTCString()}`;
+		}
+		else {
+			throw new Error("Cookie already exists");
+		}
 	}
+
+	// update cookie value
+	update(key: string, value: string, expiresMonth: number) {
+		if (this.isFound(key)) {
+			this.remove(key);
+			this.set(key, value, expiresMonth);
+		}
+		else {
+			throw new Error("Cookie not found to be updated");
+		}
+	}
+	// to remove cookie value
 	remove(key: string) {
-		return this.storage.split(";").filter((item) => {
-			return item !== key;
-		}).join(";");
+		if (this.isFound(key)) {
+			return this.storage.split(";").filter((item) => {
+				return item !== key;
+			}).join(";");
+		}
+		else {
+			throw new Error("Cookie not found to be removed");
+		}
 	}
+
 }
 
 export default new Cookie();
