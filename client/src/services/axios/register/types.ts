f67ac431c16ff6d7901a6bf38d9189cad83d3d6f
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-10-12 11:53:08 +0200

    Added register helper.

---
+++ client/src/services/axios/register/types.ts
@@ -0,0 +1,18 @@
+
+export type registeringData = {
+  first_name: string;
+  last_name: string;
+  telegram_link: string;
+  email: string;
+  birthday: Date;
+  mobile_number: string;
+  password: string;
+  location: number;
+  team: string;
+  salary: any;
+  user_type: string;
+  reporting_to: number[];
+ 
+  gender: string ;
+  job_title:string;
+};
