80cc38c9427a9f028b0027c6bf6bef159ed095ee
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-10-27 00:44:41 +0200

    Update terraform with the new result and env vars.

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,9 +29,17 @@ resource "grid_deployment" "d1" {
         flist = "https://hub.grid.tf/omda.3bot/codescalersinternship-cshr_server-latest.flist"
         cpu = 2 
         memory = 1024
-        entrypoint = "/server/start.sh"
+        entrypoint = "/sbin/zinit init"
         env_vars = {
-            SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8KhnjC8yi2Td7gMEiVmJfUNg1lXE457S+eW1jip0HW3MGLsKjXekauLbR7qcSOKUGURZlsw2KbRdOZnWbQovs24YdevUos/9XIHxFL+mJEy+5gyHuU5Z/yGcuW7O6Qj5xtwvBwe/kWnMtgEd2xXxQqEY3ZHkVh++mA/eqbhikMIsM7Qi5SKBT210+7KT5989BbUpk9e43koFNkVPCXgDR5+frhbEvCq06OVAE8vuEQ/C6EW1ZKn65nVt2z0kA7c8rUE0sEZRnI35oCEwazMlxiPm9B67GryoO7bkTvIrencFHeOrR3/7htjGxFEnJw6yyiUJtSZVP/bbRcPZ6yJtCMF03nK4IdXsHblyjXXu7u3M+7nrx6KBjew2bOHlUAU52MPOPpyJFADwM66t7P7hxIDJ3Nubwufxukqt+VasJSWI6GN9rtk6Cj1Ro2N2JJ+a4vZSdxl5RrPhujfkh87Vptxncl5G8q7oSjfMXAjk22rsJ+nuYczn57SD5PxYGjO0= mahmmoud.hassanein@gmail.com"
+            SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8KhnjC8yi2Td7gMEiVmJfUNg1lXE457S+eW1jip0HW3MGLsKjXekauLbR7qcSOKUGURZlsw2KbRdOZnWbQovs24YdevUos/9XIHxFL+mJEy+5gyHuU5Z/yGcuW7O6Qj5xtwvBwe/kWnMtgEd2xXxQqEY3ZHkVh++mA/eqbhikMIsM7Qi5SKBT210+7KT5989BbUpk9e43koFNkVPCXgDR5+frhbEvCq06OVAE8vuEQ/C6EW1ZKn65nVt2z0kA7c8rUE0sEZRnI35oCEwazMlxiPm9B67GryoO7bkTvIrencFHeOrR3/7htjGxFEnJw6yyiUJtSZVP/bbRcPZ6yJtCMF03nK4IdXsHblyjXXu7u3M+7nrx6KBjew2bOHlUAU52MPOPpyJFADwM66t7P7hxIDJ3Nubwufxukqt+VasJSWI6GN9rtk6Cj1Ro2N2JJ+a4vZSdxl5RrPhujfkh87Vptxncl5G8q7oSjfMXAjk22rsJ+nuYczn57SD5PxYGjO0= mahmmoud.hassanein@gmail.com"
+            DJANGO_SECRET_KEY="tSHKyMyXRsgfliCvjnAzySi5WI8j5f1gJSfxY7UruSq3dE814v"
+            DJANGO_DEBUG="ON"
+            EMAIL="codescalersinternship@gmail.com"
+            EMAIL_PASSWORD="ylglqynbmbcssszy"
+            EMAIL_HOST="smtp.gmail.com"
+            REDIS_HOST="redis://localhost:6379"
+            REDIS_HOST_ONLY="localhost"
+            REDIS_PORT="6379"
         }
         planetary = true
     }
