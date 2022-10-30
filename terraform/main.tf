c19e21ac267bfdf9964b43df43c752063fe855ca
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-10-30 17:17:57 +0200

    Update client Dockerfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,6 +43,17 @@ resource "grid_deployment" "d1" {
         }
         planetary = true
     }
+    vms {
+        name = "clientVM"
+        flist = "https://hub.grid.tf/omda.3bot/codescalersinternship-cshr_client-latest.flist"
+        cpu = 1
+        memory = 1024
+        entrypoint = "/sbin/zinit init"
+        env_vars = {
+            SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8KhnjC8yi2Td7gMEiVmJfUNg1lXE457S+eW1jip0HW3MGLsKjXekauLbR7qcSOKUGURZlsw2KbRdOZnWbQovs24YdevUos/9XIHxFL+mJEy+5gyHuU5Z/yGcuW7O6Qj5xtwvBwe/kWnMtgEd2xXxQqEY3ZHkVh++mA/eqbhikMIsM7Qi5SKBT210+7KT5989BbUpk9e43koFNkVPCXgDR5+frhbEvCq06OVAE8vuEQ/C6EW1ZKn65nVt2z0kA7c8rUE0sEZRnI35oCEwazMlxiPm9B67GryoO7bkTvIrencFHeOrR3/7htjGxFEnJw6yyiUJtSZVP/bbRcPZ6yJtCMF03nK4IdXsHblyjXXu7u3M+7nrx6KBjew2bOHlUAU52MPOPpyJFADwM66t7P7hxIDJ3Nubwufxukqt+VasJSWI6GN9rtk6Cj1Ro2N2JJ+a4vZSdxl5RrPhujfkh87Vptxncl5G8q7oSjfMXAjk22rsJ+nuYczn57SD5PxYGjO0= mahmmoud.hassanein@gmail.com"
+        }
+        planetary = true
+    }
 }
 
 resource "grid_deployment" "d2" {
@@ -65,6 +76,9 @@ resource "grid_deployment" "d2" {
 
 output "ygg_ip_1" {
     value = grid_deployment.d1.vms[0].ygg_ip
+}
+output "ygg_ip_1_2" {
+    value = grid_deployment.d1.vms[1].ygg_ip
 }
 output "ygg_ip_2" {
     value = grid_deployment.d2.vms[0].ygg_ip
