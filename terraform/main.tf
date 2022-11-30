fa59e979acfef4763c9e3214c64e137ad994a4be
Thunder <mahmmoud.hassanein@gmail.com>
2022-11-30 12:36:51 +0200

    Update flist.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,11 @@ provider "grid" {
     network = "dev"
 }
 
+data "grid_gateway_domain" "domain" {
+    node = 8
+    name = "cshr"
+}
+
 resource "grid_network" "net1" {
     nodes = [28, 29]
     ip_range = "10.1.0.0/16"
@@ -38,31 +43,29 @@ resource "grid_deployment" "d1" {
             EMAIL_PASSWORD="y l g l q y n b m b c s s s z y"
             EMAIL_HOST="smtp.gmail.com"
             REDIS_HOST="redis://localhost:6379"
-            REDIS_HOST_ONLY="localhost"
-            REDIS_PORT="6379"
             DJANGO_SUPERUSER_EMAIL="admin@gmail.com"
             DJANGO_SUPERUSER_PASSWORD="0000"
-            CLIENT_IP=grid_deployment.d2.vms[0].computedip
+            SERVER_DOMAIN_NAME=format("https://%s", data.grid_gateway_domain.domain.fqdn)
         }
     }
 }
 
-resource "grid_deployment" "d2" {
-  node = 29
-  network_name = grid_network.net1.name
-  ip_range = lookup(grid_network.net1.nodes_ip_range, 29, "")
-    vms {
-        name = "clientVM"
-        publicip = true
-        flist = "https://hub.grid.tf/omda.3bot/codescalersinternship-cshr_client-latest.flist"
-        cpu = 1
-        memory = 1024
-        entrypoint = "/sbin/zinit init"
-        env_vars = {
-            SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8KhnjC8yi2Td7gMEiVmJfUNg1lXE457S+eW1jip0HW3MGLsKjXekauLbR7qcSOKUGURZlsw2KbRdOZnWbQovs24YdevUos/9XIHxFL+mJEy+5gyHuU5Z/yGcuW7O6Qj5xtwvBwe/kWnMtgEd2xXxQqEY3ZHkVh++mA/eqbhikMIsM7Qi5SKBT210+7KT5989BbUpk9e43koFNkVPCXgDR5+frhbEvCq06OVAE8vuEQ/C6EW1ZKn65nVt2z0kA7c8rUE0sEZRnI35oCEwazMlxiPm9B67GryoO7bkTvIrencFHeOrR3/7htjGxFEnJw6yyiUJtSZVP/bbRcPZ6yJtCMF03nK4IdXsHblyjXXu7u3M+7nrx6KBjew2bOHlUAU52MPOPpyJFADwM66t7P7hxIDJ3Nubwufxukqt+VasJSWI6GN9rtk6Cj1Ro2N2JJ+a4vZSdxl5RrPhujfkh87Vptxncl5G8q7oSjfMXAjk22rsJ+nuYczn57SD5PxYGjO0= mahmmoud.hassanein@gmail.com"
-        }
-    }
-}
+# resource "grid_deployment" "d2" {
+#   node = 29
+#   network_name = grid_network.net1.name
+#   ip_range = lookup(grid_network.net1.nodes_ip_range, 29, "")
+#     vms {
+#         name = "clientVM"
+#         publicip = true
+#         flist = "https://hub.grid.tf/omda.3bot/codescalersinternship-cshr_client-latest.flist"
+#         cpu = 1
+#         memory = 1024
+#         entrypoint = "/sbin/zinit init"
+#         env_vars = {
+#             SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8KhnjC8yi2Td7gMEiVmJfUNg1lXE457S+eW1jip0HW3MGLsKjXekauLbR7qcSOKUGURZlsw2KbRdOZnWbQovs24YdevUos/9XIHxFL+mJEy+5gyHuU5Z/yGcuW7O6Qj5xtwvBwe/kWnMtgEd2xXxQqEY3ZHkVh++mA/eqbhikMIsM7Qi5SKBT210+7KT5989BbUpk9e43koFNkVPCXgDR5+frhbEvCq06OVAE8vuEQ/C6EW1ZKn65nVt2z0kA7c8rUE0sEZRnI35oCEwazMlxiPm9B67GryoO7bkTvIrencFHeOrR3/7htjGxFEnJw6yyiUJtSZVP/bbRcPZ6yJtCMF03nK4IdXsHblyjXXu7u3M+7nrx6KBjew2bOHlUAU52MPOPpyJFADwM66t7P7hxIDJ3Nubwufxukqt+VasJSWI6GN9rtk6Cj1Ro2N2JJ+a4vZSdxl5RrPhujfkh87Vptxncl5G8q7oSjfMXAjk22rsJ+nuYczn57SD5PxYGjO0= mahmmoud.hassanein@gmail.com"
+#         }
+#     }
+# }
 
 
 output "publicip_1" {
