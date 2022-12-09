e7b4c6e29134663ff16989a25dbde153ef1ee85a
Thunder <mahmmoud.hassanein@gmail.com>
2022-12-09 14:33:46 +0200

    Update docker client.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,12 @@ provider "grid" {
     network = "dev"
 }
 
-data "grid_gateway_domain" "domain" {
+data "grid_gateway_domain" "server_domain" {
+    node = 14
+    name = "cshrserver"
+}
+
+data "grid_gateway_domain" "client_domain" {
     node = 14
     name = "cshr"
 }
@@ -44,44 +49,61 @@ resource "grid_deployment" "d1" {
             REDIS_HOST="redis://localhost:6379"
             DJANGO_SUPERUSER_EMAIL="admin@gmail.com"
             DJANGO_SUPERUSER_PASSWORD="0000"
-            SERVER_DOMAIN_NAME=format(data.grid_gateway_domain.domain.fqdn)
-            CLIENT_DOMAIN_NAME=format(data.grid_gateway_domain.domain.fqdn)
+            SERVER_DOMAIN_NAME=format(data.grid_gateway_domain.server_domain.fqdn)
+            CLIENT_DOMAIN_NAME=format(data.grid_gateway_domain.client_domain.fqdn)
+        }
+    }
+}
+
+resource "grid_deployment" "d2" {
+    node = 14
+    network_name = grid_network.net1.name
+    vms {
+        name = "clientVM"
+        publicip = false
+        planetary = true
+        flist = "https://hub.grid.tf/omda.3bot/codescalersinternship-cshr_client-latest.flist"
+        cpu = 1
+        memory = 1024
+        entrypoint = "/sbin/zinit init"
+        env_vars = {
+            SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8KhnjC8yi2Td7gMEiVmJfUNg1lXE457S+eW1jip0HW3MGLsKjXekauLbR7qcSOKUGURZlsw2KbRdOZnWbQovs24YdevUos/9XIHxFL+mJEy+5gyHuU5Z/yGcuW7O6Qj5xtwvBwe/kWnMtgEd2xXxQqEY3ZHkVh++mA/eqbhikMIsM7Qi5SKBT210+7KT5989BbUpk9e43koFNkVPCXgDR5+frhbEvCq06OVAE8vuEQ/C6EW1ZKn65nVt2z0kA7c8rUE0sEZRnI35oCEwazMlxiPm9B67GryoO7bkTvIrencFHeOrR3/7htjGxFEnJw6yyiUJtSZVP/bbRcPZ6yJtCMF03nK4IdXsHblyjXXu7u3M+7nrx6KBjew2bOHlUAU52MPOPpyJFADwM66t7P7hxIDJ3Nubwufxukqt+VasJSWI6GN9rtk6Cj1Ro2N2JJ+a4vZSdxl5RrPhujfkh87Vptxncl5G8q7oSjfMXAjk22rsJ+nuYczn57SD5PxYGjO0= mahmmoud.hassanein@gmail.com"
+            SERVER_BASE_URL=format(data.grid_gateway_domain.server_domain.fqdn)
         }
     }
 }
 
 resource "grid_name_proxy" "p1" {
     node = 14
-    name = "cshr"
+    name = "cshrserver"
     backends = [format("http://[%s]:8000", grid_deployment.d1.vms[0].ygg_ip)]
     tls_passthrough = false
 }
 
-# resource "grid_deployment" "d2" {
-#   node = 14
-#   network_name = grid_network.net1.name
-#   ip_range = lookup(grid_network.net1.nodes_ip_range, 14, "")
-#     vms {
-#         name = "clientVM"
-#         publicip = true
-#         flist = "https://hub.grid.tf/omda.3bot/codescalersinternship-cshr_client-latest.flist"
-#         cpu = 1
-#         memory = 1024
-#         entrypoint = "/sbin/zinit init"
-#         env_vars = {
-#             SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8KhnjC8yi2Td7gMEiVmJfUNg1lXE457S+eW1jip0HW3MGLsKjXekauLbR7qcSOKUGURZlsw2KbRdOZnWbQovs24YdevUos/9XIHxFL+mJEy+5gyHuU5Z/yGcuW7O6Qj5xtwvBwe/kWnMtgEd2xXxQqEY3ZHkVh++mA/eqbhikMIsM7Qi5SKBT210+7KT5989BbUpk9e43koFNkVPCXgDR5+frhbEvCq06OVAE8vuEQ/C6EW1ZKn65nVt2z0kA7c8rUE0sEZRnI35oCEwazMlxiPm9B67GryoO7bkTvIrencFHeOrR3/7htjGxFEnJw6yyiUJtSZVP/bbRcPZ6yJtCMF03nK4IdXsHblyjXXu7u3M+7nrx6KBjew2bOHlUAU52MPOPpyJFADwM66t7P7hxIDJ3Nubwufxukqt+VasJSWI6GN9rtk6Cj1Ro2N2JJ+a4vZSdxl5RrPhujfkh87Vptxncl5G8q7oSjfMXAjk22rsJ+nuYczn57SD5PxYGjO0= mahmmoud.hassanein@gmail.com"
-#         }
-#     }
-# }
+resource "grid_name_proxy" "p2" {
+    node = 14
+    name = "cshr"
+    backends = [format("http://[%s]:8080", grid_deployment.d2.vms[0].ygg_ip)]
+    tls_passthrough = false
+}
 
 
 # output "publicip_1" {
 #     value = grid_deployment.d1.vms[0].computedip
 # }
 
-output "fqdn" {
-    value = data.grid_gateway_domain.domain.fqdn
+output "server_domain" {
+    value = data.grid_gateway_domain.server_domain.fqdn
 }
-output "ygg_ip" {
+
+output "client_domain" {
+    value = data.grid_gateway_domain.client_domain.fqdn
+}
+
+output "server_ygg_ip" {
     value = grid_deployment.d1.vms[0].ygg_ip
+}
+
+output "client_ygg_ip" {
+    value = grid_deployment.d2.vms[0].ygg_ip
 }
