f8a36358249383f5b1f4b13c686f48403d330bf2
Thunder <mahmmoud.hassanein@gmail.com>
2023-01-11 12:38:31 +0200

    Fix Issue in calander form.

--- client/src/componants/calendar/CalendarForm.svelte
+++ client/src/componants/calendar/CalendarForm.svelte
@@ -10,13 +10,13 @@
   let formToggle: number = 0;
   let thisDate: Date = new Date();
   let endDate: string;
-  let startDate = `2022-${thisDate.getMonth() + 1}-${thisDate.getDate()}`;
+  let startDate = `${thisDate.getFullYear()}-${thisDate.getMonth() + 1}-${thisDate.getDate()}`;
   const daysInCurrentMonth = getDaysInMonth(thisDate.getFullYear(), thisDate.getMonth() + 1);
 
   if (thisDate.getDate() + 2 > daysInCurrentMonth){
-    endDate = `2022-${thisDate.getMonth() + 2}-${thisDate.getDate() + 2 - daysInCurrentMonth}`;
+    endDate = `${thisDate.getFullYear()}-${thisDate.getMonth() + 2}-${thisDate.getDate() + 2 - daysInCurrentMonth}`;
   } else {
-    endDate = `2022-${thisDate.getMonth() + 1}-${thisDate.getDate() + 2}`;
+    endDate = `${thisDate.getFullYear()}-${thisDate.getMonth() + 1}-${thisDate.getDate() + 2}`;
   }
   
   let datePickerDisabled = false;
