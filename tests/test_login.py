ac5569cd2976da091b35a79298c78f18e31b8d44
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2023-08-22 10:25:19 +0300

    remove unrequired files

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,48 +1,52 @@
-from pages.login import login
-from tests.contest import driver
-import utils 
 import time
-
-def test_user_login(driver):
+from pages.login import login
+import pytest
+from selenium import webdriver
+@pytest.fixture
+def driver1():
+    driver = webdriver.Chrome()
+    driver.get("https://cshr.gent01.dev.grid.tf")
+    yield driver
+    driver.quit()
+def test_user_login(driver1):
     login_instance = login()
     # Enter email
-    email_input = driver.find_element(*login_instance.email)
+    email_input = driver1.find_element(*login_instance.email)
     email_input.send_keys("A@test.cs")
     # Enter password
-    password_input = driver.find_element(*login_instance.password)
+    password_input = driver1.find_element(*login_instance.password)
     password_input.send_keys("123456789")
      # Click on Login button
-    login_button = driver.find_element(*login_instance.login_button)
+    login_button = driver1.find_element(*login_instance.login_button)
     login_button.click()
 
-    time.sleep(20)
+    time.sleep(10)
     # Get the  profile
-    profile = driver.find_element(*login_instance.profile)
+    profile = driver1.find_element(*login_instance.profile)
     # Assert that the element includes the text "AW"
     assert "aw" in profile.text.lower()
-def test_invalid_login(driver):
+def test_invalid_login(driver1):
     login_instance = login()
     # Enter email invalid email
-    email_input = driver.find_element(*login_instance.email)
+    email_input = driver1.find_element(*login_instance.email)
     email_input.send_keys("invalid@email.com")
     
     # Enter invalid password
-    password_input = driver.find_element(*login_instance.password)
+    password_input = driver1.find_element(*login_instance.password)
     password_input.send_keys("123456789")
     
     # Click on Login button
-    login_button = driver.find_element(*login_instance.login_button)
-    try:
-       login_button.click()
-    except Exception as e:
-        assert "element not clickable" 
-def test_email_validation(driver):
+    login_button = driver1.find_element(*login_instance.login_button)
+    try:login_button.click()
+    except ImportError:
+        assert True 
+def test_email_validation(driver1):
     login_instance = login()
     # Enter email invalid email
-    email_input = driver.find_element(*login_instance.email)
+    email_input = driver1.find_element(*login_instance.email)
     email_input.send_keys("invalid@email.com")
     # get the error message
-    undefined_message=driver.find_element(*login_instance.undefined_message)
+    undefined_message=driver1.find_element(*login_instance.undefined_message)
     actual_text = undefined_message.text
     expected_text = "undefined"
     assert actual_text == expected_text
