1f0d770995236e7a9d4d7e81610fc7f5780d3edf
aliwaleed1 <138436398+aliwaleed1@users.noreply.github.com>
2023-08-23 19:32:37 +0300

    Add files via upload

--- pages/meeting.py
+++ pages/meeting.py
@@ -1,22 +1,42 @@
+"""page objects."""
 from selenium.webdriver.common.by import By
 
-class login:
+
+class Login:
+    """login class."""
     email = (By.XPATH, '//*[@type="email"]')
     password = (By.XPATH, '//*[@type="password"]')
     login_button = (By.XPATH, '//button[text()="Login"]')
-    profile = (By.XPATH, '//span[@class="user-profile-image rounded-circle svelte-18t1c4h" and contains(text(), "AW")]')
     undefined_message = (By.XPATH, '//*[@id="liveToast"]/div[2]')
-class meeting:
-    meeting_button=(By.XPATH,'//*[@id="isloading"]/div[2]/div[1]/div/button[2]')
-    Start_Date = (By.XPATH,'//input[@placeholder="Start Date"]')
-    combo_box = (By.XPATH, '//div[@class="select-container my-2 svelte-1v9yc5w"]')
-    selected_people = (By.XPATH, '//*[@id="isloading"]/div[2]/div[2]/div[2]/form/div[1]/div/div/ul/li[2]/div/div/div/span')
-    location = (By.XPATH,'//input[@placeholder="Meeting Location"]')
-    fill_button = (By.XPATH,'//*[@id="isloading"]/div[2]/div[2]/div[2]/form/div[3]/button')
-    Link = (By.XPATH,'//input[@placeholder="Meeting Link"]')
-    meeting_time = (By.XPATH,'//input[@type="time"]')
-    submit_button = (By.XPATH,'//*[@type="submit"]')
-    random_day = (By.XPATH,'//*[@id="highlight"]/table/tr[5]/td[6]')
-    date_error_message = (By.XPATH,'/html/body/div[4]/div[2]/div/div/div/div[1]/div[2]/div/fieldset/div[2]/div[2]/div[1]/div/div[1]/span')
-    delete_people_button = (By.XPATH,'//*[@id="isloading"]/div[2]/div[2]/div[2]/form/div[1]/div/div/div[1]/button')
-    Link_error_message  =(By.XPATH,'/html/body/div[4]/div[2]/div/div/div/div[1]/div[2]/div/fieldset/div[2]/div[2]/div[2]/div/div/div/div[2]/form/div[1]/div/div[1]/span')
+
+
+class Meeting:
+    """Meeting class."""
+    xpath_part1 = '//*[@id="isloading"]/div[2]'
+    xpath_part2 = '/div[1]/div/button[2]'
+    meeting_button = (By.XPATH, xpath_part1 + xpath_part2)
+    Start_Date = (By.XPATH, '//input[@placeholder="Start Date"]')
+    xpath_partcomb1 = '//div[@class="select-container'
+    xpath_partcomb2 = ' my-2 svelte-1v9yc5w"]'
+    combo_box = (By.XPATH, xpath_partcomb1+xpath_partcomb2)
+    xpath_part3 = '//*[@id="isloading"]/div[2]/div[2]/div[2]/form/div[1]'
+    xpath_part4 = '/div/div/ul/li[2]/div/div/div/span'
+    selected_people = (By.XPATH, xpath_part3 + xpath_part4)
+    location = (By.XPATH, '//input[@placeholder="Meeting Location"]')
+    xpath_part5 = '//*[@id="isloading"]/div[2]/div[2]'
+    xpath_part6 = '/div[2]/form/div[3]/button'
+    fill_button = (By.XPATH, xpath_part5 + xpath_part6)
+    Link = (By.XPATH, '//input[@placeholder="Meeting Link"]')
+    meeting_time = (By.XPATH, '//input[@type="time"]')
+    submit_button = (By.XPATH, '//*[@type="submit"]')
+    random_day = (By.XPATH, '//*[@id="highlight"]/table/tr[5]/td[6]')
+    xpath_part7 = '/html/body/div[4]/div[2]/div/div/div/div[1]/div[2]'
+    xpath_part8 = '/div/fieldset/div[2]/div[2]/div[1]/div/div[1]/span'
+    date_error_message = (By.XPATH, xpath_part7 + xpath_part8)
+    xpath_part9 = '//*[@id="isloading"]/div[2]/div[2]/div[2]'
+    xpath_part10 = '/form/div[1]/div/div/div[1]/button'
+    delete_people_button = (By.XPATH, xpath_part9 + xpath_part10)
+    xpath_part11 = '/html/body/div[4]/div[2]/div/div/div/'
+    xpath_part12 = 'div[1]/div[2]/div/fieldset/div[2]/div[2]/div[2]'
+    xpath_part13 = '/div/div/div/div[2]/form/div[1]/div/div[1]/span'
+    Link_error_message = (By.XPATH, xpath_part11 + xpath_part12 + xpath_part13)
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,175 +1,217 @@
+"""Module to test meeting page."""
 import time
-from pages.meeting import login ,meeting
-import pytest
 from selenium import webdriver
+import pytest
+from pages.meeting import Login, Meeting
 from utils.Base import base_url
-@pytest.fixture
-def driver1():
-    driver = webdriver.Chrome()
+
+
+@pytest.fixture(name="driver1")
+def driver1_fixture():
+    """Function to save driver fixture."""
+    driver = webdriver.Edge()
     driver.get(base_url)
     yield driver
     driver.quit()
+
+
 def to_login(driver1):
-    email_input = driver1.find_element(*login.email)
+    """Function to login in tests."""
+    email_input = driver1.find_element(*Login.email)
     email_input.send_keys("A@test.cs")
-    password_input = driver1.find_element(*login.password)
+    password_input = driver1.find_element(*Login.password)
     password_input.send_keys("123456789")
-    login_button = driver1.find_element(*login.login_button)
+    login_button = driver1.find_element(*Login.login_button)
     login_button.click()
     time.sleep(3)
-def test_Creation_Meeting(driver1):
+
+
+def test_creation_meeting(driver1):
+    """test for creation meeting."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
     # Write start date
-    Start_Date = driver1.find_element(*meeting.Start_Date)
-    Start_Date.clear()
-    Start_Date.send_keys("2023-08-24")
+    start_date = driver1.find_element(*Meeting.Start_Date)
+    start_date.clear()
+    start_date.send_keys("2023-08-24")
     # Click on the  people combo box
-    combo_box = driver1.find_element(*meeting.combo_box)
+    combo_box = driver1.find_element(*Meeting.combo_box)
     combo_box.click()
     # select people
-    selected_people = driver1.find_element(*meeting.selected_people)
+    selected_people = driver1.find_element(*Meeting.selected_people)
     selected_people.click()
     # Enter the meeting location
-    location = driver1.find_element(*meeting.location)
+    location = driver1.find_element(*Meeting.location)
     location.send_keys("online")
     # Click the fill button
-    fill_button = driver1.find_element(*meeting.fill_button)
-    try:fill_button.click()
+    fill_button = driver1.find_element(*Meeting.fill_button)
+    try:
+        fill_button.click()
     except ImportError:
-        assert True 
-    # Enter the meeting link
-    Link = driver1.find_element(*meeting.Link)
-    Link.send_keys("www.com")
+        assert True
+    # Enter the meeting linkf
+    link = driver1.find_element(*Meeting.Link)
+    link.send_keys("www.com")
     # Configure the meeting time input
-    meeting_time = driver1.find_element(*meeting.meeting_time)
-    driver1.execute_script("arguments[0].setAttribute('autocomplete', 'off')",meeting_time)
+    meeting_time = driver1.find_element(*Meeting.meeting_time)
+    script = "arguments[0].setAttribute('autocomplete', 'off')"
+    driver1.execute_script(script, meeting_time)
     meeting_time.clear()
     meeting_time.send_keys("12:30PM")
     # Click the submit button
-    submit_button = driver1.find_element(*meeting.submit_button)
-    try:submit_button.click()
+    submit_button = driver1.find_element(*Meeting.submit_button)
+    try:
+        submit_button.click()
     except ImportError:
         assert True
-def test_Selection_Date_from_Calendar(driver1):
+
+
+def test_selection_date_from_calendar(driver1):
+    """test for selection date from calendar."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
-    #Select day from calendar
-    random_day=driver1.find_element(*meeting.random_day)
+    # Select day from calendar
+    random_day = driver1.find_element(*Meeting.random_day)
     random_day.click()
-    Start_Date = driver1.find_element(*meeting.Start_Date)
-    input_value = Start_Date.get_attribute("value")
+    start_date = driver1.find_element(*Meeting.Start_Date)
+    input_value = start_date.get_attribute("value")
     assert input_value == "2023-08-25"
-@pytest.mark.parametrize("invalid_dates", ["2023-08-99", "33333-08-12", "2023-88-99", "0000-00-00"])
-def test_Date_Format_validation(driver1,invalid_dates):
+
+
+@pytest.mark.parametrize("invalid_dates", [
+    "2023-08-99",
+    "33333-08-12",
+    "2023-88-99",
+    "0000-00-00"
+])
+def test_date_format_validation(driver1, invalid_dates):
+    """test for date format validation with more than one date."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
     # Write invalid start date
-    Start_Date = driver1.find_element(*meeting.Start_Date)
-    Start_Date.clear()
-    Start_Date.send_keys(invalid_dates)
+    start_date = driver1.find_element(*Meeting.Start_Date)
+    start_date.clear()
+    start_date.send_keys(invalid_dates)
     time.sleep(3)
-    date_error_message = driver1.find_element(*meeting.date_error_message)
+    date_error_message = driver1.find_element(*Meeting.date_error_message)
     assert "Success!" != date_error_message.text
-def test_Missing_Attendee(driver1):
+
+
+def test_missing_attendee(driver1):
+    """test for missing attendee."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
     # Write start date
-    Start_Date = driver1.find_element(*meeting.Start_Date)
-    Start_Date.clear()
-    Start_Date.send_keys("2023-08-24")
+    start_date = driver1.find_element(*Meeting.Start_Date)
+    start_date.clear()
+    start_date.send_keys("2023-08-24")
     # Enter the meeting location
-    location = driver1.find_element(*meeting.location)
+    location = driver1.find_element(*Meeting.location)
     location.send_keys("online")
     # Click the fill button
-    fill_button = driver1.find_element(*meeting.fill_button)
-    assert  not fill_button.is_enabled()
-def test_Missing_location(driver1):
+    fill_button = driver1.find_element(*Meeting.fill_button)
+    assert not fill_button.is_enabled()
+
+
+def test_missing_location(driver1):
+    """test for missing location."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
     # Write start date
-    Start_Date = driver1.find_element(*meeting.Start_Date)
-    Start_Date.clear()
-    Start_Date.send_keys("2023-08-24")
+    start_date = driver1.find_element(*Meeting.Start_Date)
+    start_date.clear()
+    start_date.send_keys("2023-08-24")
     # Click on the  people combo box
-    combo_box = driver1.find_element(*meeting.combo_box)
+    combo_box = driver1.find_element(*Meeting.combo_box)
     combo_box.click()
     # select people
-    selected_people = driver1.find_element(*meeting.selected_people)
+    selected_people = driver1.find_element(*Meeting.selected_people)
     selected_people.click()
     # Click the fill button
-    fill_button = driver1.find_element(*meeting.fill_button)
-    assert  not fill_button.is_enabled()
+    fill_button = driver1.find_element(*Meeting.fill_button)
+    assert not fill_button.is_enabled()
+
+
 def test_remove_someone_in_people(driver1):
+    """test for remove someone in people."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
     # Write start date
-    Start_Date = driver1.find_element(*meeting.Start_Date)
-    Start_Date.clear()
-    Start_Date.send_keys("2023-08-24")
+    start_date = driver1.find_element(*Meeting.Start_Date)
+    start_date.clear()
+    start_date.send_keys("2023-08-24")
     # Click on the  people combo box
-    combo_box = driver1.find_element(*meeting.combo_box)
+    combo_box = driver1.find_element(*Meeting.combo_box)
     combo_box.click()
     # select people
-    selected_people = driver1.find_element(*meeting.selected_people)
+    selected_people = driver1.find_element(*Meeting.selected_people)
     selected_people.click()
     # Enter the meeting location
-    location = driver1.find_element(*meeting.location)
+    location = driver1.find_element(*Meeting.location)
     location.send_keys("online")
     # Click the fill button
-    fill_button = driver1.find_element(*meeting.fill_button)
-    assert  fill_button.is_enabled()
-    delete_people_button = driver1.find_element(*meeting.delete_people_button)
+    fill_button = driver1.find_element(*Meeting.fill_button)
+    assert fill_button.is_enabled()
+    delete_people_button = driver1.find_element(*Meeting.delete_people_button)
     delete_people_button.click()
     assert not fill_button.is_enabled()
-def test_Missing_Meeting_Link(driver1):
+
+
+def test_missing_meeting_link(driver1):
+    """test for missing meeting link."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
+    time.sleep(4)
     # Write start date
-    Start_Date = driver1.find_element(*meeting.Start_Date)
-    Start_Date.clear()
-    Start_Date.send_keys("2023-08-24")
+    start_date = driver1.find_element(*Meeting.Start_Date)
+    start_date.clear()
+    start_date.send_keys("2023-08-25")
+    time.sleep(4)
     # Click on the  people combo box
-    combo_box = driver1.find_element(*meeting.combo_box)
+    combo_box = driver1.find_element(*Meeting.combo_box)
     combo_box.click()
     # select people
-    selected_people = driver1.find_element(*meeting.selected_people)
+    selected_people = driver1.find_element(*Meeting.selected_people)
     selected_people.click()
     # Enter the meeting location
-    location = driver1.find_element(*meeting.location)
+    location = driver1.find_element(*Meeting.location)
     location.send_keys("online")
-    time.sleep(2)
+    time.sleep(4)
     # Click the fill button
-    fill_button = driver1.find_element(*meeting.fill_button)
+    fill_button = driver1.find_element(*Meeting.fill_button)
     fill_button.click()
     # Configure the meeting time input
-    meeting_time = driver1.find_element(*meeting.meeting_time)
-    driver1.execute_script("arguments[0].setAttribute('autocomplete', 'off')",meeting_time)
+    meeting_time = driver1.find_element(*Meeting.meeting_time)
+    script = "arguments[0].setAttribute('autocomplete', 'off')"
+    driver1.execute_script(script, meeting_time)
     meeting_time.clear()
     meeting_time.send_keys("12:30PM")
-     # Click the submit button
-    submit_button = driver1.find_element(*meeting.submit_button)
+    time.sleep(4)
+    # Click the submit button
+    submit_button = driver1.find_element(*Meeting.submit_button)
     assert not submit_button.is_enabled()
-def test_Meeting_Link_validation(driver1):
+
+
+def test_meeting_link_validation(driver1):
+    """test for meeting link validation."""
     to_login(driver1)
     # Click on the meeting button
-    meeting_button=driver1.find_element(*meeting.meeting_button)
+    meeting_button = driver1.find_element(*Meeting.meeting_button)
     meeting_button.click()
     time.sleep(2)
-    Link_error_message = driver1.find_element(*meeting.Link_error_message)
-    assert "Meeting Link is invalid" == Link_error_message.text
-    
+    link_error_message = driver1.find_element(*Meeting.Link_error_message)
+    assert "Meeting Link is invalid" == link_error_message.text
