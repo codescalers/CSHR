c93696f254bb66cf10983ca647d4fd8323e84172
aliwaleed1 <138436398+aliwaleed1@users.noreply.github.com>
2023-08-28 06:16:21 +0300

    Add files via upload

--- pages/vacation.py
+++ pages/vacation.py
@@ -1,6 +1,8 @@
 """page objects."""
 import random
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Login:
@@ -10,16 +12,32 @@ class Login:
     login_button = (By.XPATH, '//button[text()="Login"]')
     undefined_message = (By.XPATH, '//*[@id="liveToast"]/div[2]')
 
+    def __init__(self, browser):
+        self.browser = browser
+
+    def enter_email(self, email):
+        email_input = self.browser.find_element(*self.email)
+        email_input.clear()
+        email_input.send_keys(email)
+
+    def enter_password(self, password):
+        password_input = self.browser.find_element(*self.password)
+        password_input.clear()
+        password_input.send_keys(password)
+
+    def click_login_button(self):
+        return self.browser.find_element(*self.login_button)
+
 
 class Vacation:
     """Vacation class."""
     dashboard_button = (By.XPATH, "//a[contains(@href, '/dashboard')]")
-    users_button = (By.XPATH, "//div[@class='caret svelte-1v9yc5w']")
     xpath_for_section = '/html/body/div[20]/div[2]/section/div/div[2]/div/'
     random_userx2 = 'div[2]/div/div/div/div/div/div/div/ul/li[3]/div/div/div'
     random_user = (By.XPATH, xpath_for_section + random_userx2)
     random_user_namex1 = '//*[@id="v-tabs-user-vacation-balance"]/div/div/'
     random_user_namex2 = 'div/div[1]/div/div/div/div[1]/button'
+    users_button = (By.XPATH, random_user_namex1 + 'div/div/div/div/div')
     random_user_name = (By.XPATH, random_user_namex1 + random_user_namex2)
     load_balanc_butt = (By.XPATH, "//button[contains(text(), 'Load Balance')]")
     annual_leavesx2 = 'div[2]/div/div/div/div/div[2]/div/div/input'
@@ -39,6 +57,98 @@ class Vacation:
     leave_error_message = (By.XPATH, error_messages+leave_error)
     emergency_error = 'div/div/div/div/div[4]/div/div/div[1]/span'
     emergency_error_message = (By.XPATH, error_messages+emergency_error)
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def click_dashboard_button(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.dashboard_button))
+        dashboard_button = self.browser.find_element(*self.dashboard_button)
+        dashboard_button.click()
+
+    def click_users_button(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.users_button))
+        users_button = self.browser.find_element(*self.users_button)
+        users_button.click()
+
+    def click_random_user(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.random_user))
+        random_user = self.browser.find_element(*self.random_user)
+        random_user.click()
+
+    def click_load_balance_button(self):
+        load_balanc_button = self.browser.find_element(*self.load_balanc_butt)
+        load_balanc_button.click()
+
+    def enter_leave_execuses(self, leave_number):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.leave_execuses))
+        leave_execuses = self.browser.find_element(*self.leave_execuses)
+        leave_execuses.click()
+        leave_execuses.send_keys(leave_number)
+
+    def enter_annual_leaves(self, annual_number):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.annual_leaves))
+        annual_leaves = self.browser.find_element(*self.annual_leaves)
+        annual_leaves.click()
+        annual_leaves.send_keys(annual_number)
+
+    def enter_emergency_leaves(self, emergency_number):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.emergency_leaves))
+        emergency_leaves = self.browser.find_element(*self.emergency_leaves)
+        emergency_leaves.click()
+        emergency_leaves.send_keys(emergency_number)
+
+    def click_delete_old_balance_button(self):
+        button = self.browser.find_element(*self.delete_old_balance_button)
+        button.click()
+
+    def click_submit_button(self):
+        submit_button = self.browser.find_element(*self.submit_button)
+        submit_button.click()
+
+    def get_random_user_name(self):
+        return self.browser.find_element(*Vacation.random_user_name)
+
+    def get_load_balanc_button(self):
+        return self.browser.find_element(*Vacation.load_balanc_butt)
+
+    def get_user_after_selected(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.user_name_selected))
+        return self.browser.find_element(*Vacation.user_name_selected)
+
+    def get_annual_error_message(self):
+        return self.browser.find_element(*Vacation.annual_error_message)
+
+    def get_leave_error_message(self):
+        return self.browser.find_element(*Vacation.leave_error_message)
+
+    def get_emergency_error_message(self):
+        return self.browser.find_element(*Vacation.emergency_error_message)
+
+    def get_annual_leaves(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.annual_leaves))
+        return self.browser.find_element(*Vacation.annual_leaves)
+
+    def get_leave_execuses(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.leave_execuses))
+        return self.browser.find_element(*Vacation.leave_execuses)
+
+    def get_emergency_leaves(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.emergency_leaves))
+        return self.browser.find_element(*Vacation.emergency_leaves)
+
+    def get_submit_button(self):
+        return self.browser.find_element(*Vacation.submit_button)
 
 
 class Employees:
@@ -63,4 +173,35 @@ class Employees:
     leave_execuses_text1 = 'div/div[2]/div[4]/div[2]/div[2]/div[3]'
     leave_execus_text = (By.XPATH, profile_text + leave_execuses_text1)
     emergency_leaves_text1 = '/div[2]/div[4]/div[2]/div[2]/div[2]'
-    emergency_leaves_text = (By.XPATH, profile_text + emergency_leaves_text1)
+    emergency_leave_text = (By.XPATH, profile_text + emergency_leaves_text1)
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def click_employees_button(self):
+        employees_button = self.browser.find_element(*self.employees_button)
+        employees_button.click()
+
+    def click_random_profile(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.random_profile))
+        random_profile = self.browser.find_element(*self.random_profile)
+        random_profile.click()
+
+    def get_annual_leaves_text(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.annual_leaves_text))
+        annual_text = self.browser.find_element(*self.annual_leaves_text)
+        return annual_text.text
+
+    def get_emergency_leaves_text(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.emergency_leave_text))
+        emergency_text = self.browser.find_element(*self.emergency_leave_text)
+        return emergency_text.text
+
+    def get_leave_execus_text(self):
+        wait = WebDriverWait(self.browser, 30)
+        wait.until(EC.visibility_of_element_located(self.leave_execus_text))
+        leave_text = self.browser.find_element(*self.leave_execus_text)
+        return leave_text.text
--- tests/test_vacation.py
+++ tests/test_vacation.py
@@ -1,102 +1,74 @@
 """Module to test Update User Vacation Balance functionality."""
 import time
-from selenium import webdriver
 import pytest
 from pages.vacation import Login, Vacation, Employees
-from utils.base import BASEURL
 
 
-@pytest.fixture(name="driver1")
-def driver1_fixture():
-    """Function to save driver fixture."""
-    driver = webdriver.Edge()
-    driver.get(BASEURL)
-    yield driver
-    driver.quit()
-
-
-def to_login(driver1):
+def to_login(browse):
     """Function to login in tests."""
-    email_input = driver1.find_element(*Login.email)
-    email_input.send_keys("A@test.cs")
-    password_input = driver1.find_element(*Login.password)
-    password_input.send_keys("123456789")
-    login_button = driver1.find_element(*Login.login_button)
+    login_page = Login(browse)
+    login_page.enter_email("A@test.cs")
+    login_page.enter_password("123456789")
+    login_button = login_page.click_login_button()
     login_button.click()
-    time.sleep(3)
 
 
-def to_select_user(driver1):
+def to_select_user(browse):
     """Function to select user in tests."""
-    time.sleep(3)
-    dashboard_button = driver1.find_element(*Vacation.dashboard_button)
-    dashboard_button.click()
-    time.sleep(3)
-    button = driver1.find_element(*Vacation.users_button)
-    button.click()
-    time.sleep(3)
-    random_user = driver1.find_element(*Vacation.random_user)
-    random_user.click()
+    vacation_page = Vacation(browse)
+    vacation_page.click_dashboard_button()
+    vacation_page.click_users_button()
+    vacation_page.click_random_user()
 
 
-def test_update_of_user_vacation_balance(driver1):
+def test_update_of_user_vacation_balance(browse):
     """Function to test update of user vacation balance."""
-    to_login(driver1)
+    to_login(browse)
     time.sleep(3)
-    to_select_user(driver1)
-    load_balanc_button = driver1.find_element(*Vacation.load_balanc_butt)
-    load_balanc_button.click()
-    time.sleep(3)
-    leave_execuses = driver1.find_element(*Vacation.leave_execuses)
-    leave_execuses.click()
-    leave_execuses.send_keys(Employees.leave_number)
-    annual_leaves = driver1.find_element(*Vacation.annual_leaves)
-    annual_leaves.click()
-    annual_leaves.send_keys(Employees.annual_number)
-    emergency_leaves = driver1.find_element(*Vacation.emergency_leaves)
-    emergency_leaves.click()
-    emergency_leaves.send_keys(Employees.emergency_number)
-    delete_button = driver1.find_element(*Vacation.delete_old_balance_button)
-    delete_button.click()
-    time.sleep(3)
-    submit_button = driver1.find_element(*Vacation.submit_button)
-    submit_button.click()
-    # go to employees page to check update
-    employees_button = driver1.find_element(*Employees.employees_button)
-    employees_button.click()
-    time.sleep(3)
-    random_profile = driver1.find_element(*Employees.random_profile)
-    random_profile.click()
-    time.sleep(3)
-    annual_text = driver1.find_element(*Employees.annual_leaves_text)
-    emergency_text = driver1.find_element(*Employees.emergency_leaves_text)
-    leave_text = driver1.find_element(*Employees.leave_execus_text)
-    is_annual = annual_text.text == Employees.annual_string
-    is_emergency = emergency_text.text == Employees.emergency_string
-    is_leave = leave_text.text == Employees.leave_string
+    to_select_user(browse)
+    vacation_page = Vacation(browse)
+    vacation_page.click_load_balance_button()
+    vacation_page.enter_leave_execuses(Employees.leave_number)
+    vacation_page.enter_annual_leaves(Employees.annual_number)
+    vacation_page.enter_emergency_leaves(Employees.emergency_number)
+    vacation_page.click_delete_old_balance_button()
+    vacation_page.click_submit_button()
+
+    employees_page = Employees(browse)
+    employees_page.click_employees_button()
+
+    employees_page.click_random_profile()
+
+    annual_text = employees_page.get_annual_leaves_text()
+    emergency_text = employees_page.get_emergency_leaves_text()
+    leave_text = employees_page.get_leave_execus_text()
+
+    is_annual = annual_text == Employees.annual_string
+    is_emergency = emergency_text == Employees.emergency_string
+    is_leave = leave_text == Employees.leave_string
+
     assert is_annual and is_emergency and is_leave
 
 
-def test_name_of_the_user(driver1):
+def test_name_of_the_user(browse):
     """Function to test name of selected user."""
-    to_login(driver1)
-    time.sleep(3)
-    to_select_user(driver1)
-    random_user_name = driver1.find_element(*Vacation.random_user_name)
+    to_login(browse)
+    to_select_user(browse)
+    vacation_page = Vacation(browse)
+    random_user_name = vacation_page.get_random_user_name()
     user_name_in_selected_button = random_user_name.text
-    load_balanc_button = driver1.find_element(*Vacation.load_balanc_butt)
+    load_balanc_button = vacation_page.get_load_balanc_button()
     load_balanc_button.click()
-    time.sleep(3)
-    user_after_selected = driver1.find_element(*Vacation.user_name_selected)
+    user_after_selected = vacation_page.get_user_after_selected()
     assert user_after_selected.text in user_name_in_selected_button
 
 
-def test_check_load_balance_button(driver1):
+def test_check_load_balance_button(browse):
     """Function to test load balance button."""
-    to_login(driver1)
-    time.sleep(3)
-    to_select_user(driver1)
-    load_balanc_button = driver1.find_element(*Vacation.load_balanc_butt)
+    to_login(browse)
+    to_select_user(browse)
+    vacation_page = Vacation(browse)
+    load_balanc_button = vacation_page.get_load_balanc_button()
     assert load_balanc_button.is_enabled()
     assert load_balanc_button.text == "Load Balance"
 
@@ -106,32 +78,34 @@ def test_check_load_balance_button(driver1):
     ("789", "", "456"),
     ("789", "123", ""),
 ])
-def test_fields_with_empty(driver1, annual, leave, emergency):
+def test_fields_with_empty(browse, annual, leave, emergency):
     """Function to test load balance button."""
-    to_login(driver1)
-    to_select_user(driver1)
-    load_balanc_button = driver1.find_element(*Vacation.load_balanc_butt)
+    to_login(browse)
+    to_select_user(browse)
+    vacation_page = Vacation(browse)
+    load_balanc_button = vacation_page.get_load_balanc_button()
     load_balanc_button.click()
-    time.sleep(3)
-    annual_leaves = driver1.find_element(*Vacation.annual_leaves)
+    annual_leaves = vacation_page.get_annual_leaves()
     annual_leaves.click()
     annual_leaves.send_keys(annual)
-    leave_execuses = driver1.find_element(*Vacation.leave_execuses)
+    leave_execuses = vacation_page.get_leave_execuses()
     leave_execuses.click()
     leave_execuses.send_keys(leave)
-    emergency_leaves = driver1.find_element(*Vacation.emergency_leaves)
+    emergency_leaves = vacation_page.get_emergency_leaves()
     emergency_leaves.click()
     emergency_leaves.send_keys(emergency)
-    submit_button = driver1.find_element(*Vacation.submit_button)
+    submit_button = vacation_page.get_submit_button()
     if annual == "":
-        error_message = driver1.find_element(*Vacation.annual_error_message)
+        error_message = vacation_page.get_annual_error_message()
         assert "Annual leaves is invalid" in error_message.text
         assert not submit_button.is_enabled()
+
     if leave == "":
-        error_message = driver1.find_element(*Vacation.leave_error_message)
+        error_message = vacation_page.get_leave_error_message()
         assert "Leave execuses is invalid" in error_message.text
         assert not submit_button.is_enabled()
+
     if emergency == "":
-        error_message = driver1.find_element(*Vacation.emergency_error_message)
+        error_message = vacation_page.get_emergency_error_message()
         assert "Emergency leaves is invalid" in error_message.text
         assert not submit_button.is_enabled()
