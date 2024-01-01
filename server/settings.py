87b851b40116b71b523e57acb7d4af51aaefc29f
Thunder <mahmmoud.hassanein@gmail.com>
2024-01-01 10:33:08 +0200

    Implement Postgres Backend with Isolated Container for Improved Docker Compose Management. (#263)
    * Added the postgres configuration, created a container in the docker compose file.
    
    * Deleted the migration since we changed the DB to Postgres DB.
    
    * Added required files, make lint.

--- server/settings.py
+++ server/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DATABASE_NAME"),
+        "USER": config("DATABASE_USER"),
+        "PASSWORD": config("DATABASE_PASSWORD"),
+        "HOST": config("DATABASE_HOST"),
+        "PORT": config("DATABASE_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
