2d7e00dfa2c24b1f055905535c52a327be17ac28
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-01-01 13:49:37 +0200

    Added new env var for the development process.

--- server/settings.py
+++ server/settings.py
@@ -83,6 +83,16 @@ DATABASES = {
     }
 }
 
+if config("ENV") == "development":
+    # use the sqlite db for development process.
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
