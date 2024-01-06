832a3113bcfd4b845f3852648f824ec59be78286
zaelgohary <zainabelgohary19@gmail.com>
2024-01-06 23:46:41 +0200

    Add login page

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,12 +1,99 @@
 <template>
-  <p>This is Login</p>
+  <v-row no-gutters class="fill-height" align="center" justify="center">
+    <v-col cols="7">
+      <v-card
+        class="fill-hight pa-16"
+        :image="background"
+        height="100vh"
+        title="CSHR (Codescalers HR Management System)"
+        subtitle="A versatile management system with vacation request submissions and seamless integration with a global calendar. Users across offices can collectively view approved vacations, Birthdates, Events, Meetings, and Holidays, providing a centralized overview for the entire organization."
+      ></v-card>
+    </v-col>
+    <v-col cols="5">
+      <v-form ref="form" @submit.prevent="login(email, password)">
+        <v-img :src="logo" max-width="200" class="mx-auto justify-center"></v-img>
+        <h1 class="text-center my-5">Sign In</h1>
+        <v-row class="justify-center align-center">
+          <v-col cols="7">
+            <v-text-field v-model="email" label="Email" :rules="emailRules"></v-text-field>
+          </v-col>
+
+          <v-col cols="7">
+            <v-text-field
+              v-model="password"
+              :append-inner-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="passwordRules"
+              :type="show ? 'text' : 'password'"
+              label="Password"
+              @click:append-inner="show = !show"
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="7">
+            <v-btn color="primary" type="submit" :disabled="!form?.isValid" width="100%"
+              >Login</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
-export default {
-  name: "LoginView",
-  setup() {
-  }
-}
-</script>
+import { defineComponent, onMounted, ref } from 'vue'
+import background from '../assets/login.png'
+import logo from '../assets/cshr_logo.png'
+import { $api } from '@/clients'
+import { test_auth_api } from '@/tests/api/auth'
+import { emailRules, passwordRules } from '@/utils'
+import { useRouter } from 'vue-router'
 
+export default defineComponent({
+  setup() {
+    const form = ref()
+    const email = ref<string>('')
+    const password = ref<string>('')
+    const show = ref(false)
+    const $router = useRouter()
+
+    onMounted(async () => {
+      const loggedUser = await test_auth_api();
+      console.log({loggedUser})
+    })
+    async function login(email: string, password: string) {
+      await $api.auth.login({
+        email,
+        password
+      })
+      $router.push("/")
+    }
+
+    return {
+      form,
+      email,
+      password,
+      show,
+      login,
+      emailRules,
+      passwordRules,
+      background,
+      logo
+    }
+  }
+})
+</script>
+<style scoped>
+:deep(.v-card-title) {
+  margin-top: 20% !important;
+  font-weight: bold !important;
+  font-size: 1.5rem !important;
+}
+
+:deep(.v-card-subtitle) {
+  margin-top: 1rem !important;
+  font-size: 1rem !important;
+  white-space: inherit !important;
+  opacity: 1 !important;
+}
+</style>
