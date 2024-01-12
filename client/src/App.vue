4b47dd52bb1e1a4adcdc875465230267d76260c6
zaelgohary <zainabelgohary19@gmail.com>
2024-01-12 17:22:47 +0200

    fix: assign state value on app mount

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,6 +9,8 @@ import { useNotifier } from 'vue3-notifier'
 import { useApi } from '@/hooks'
 import SideDrawer from './components/SideDrawer.vue'
 import { test_api } from '@/tests'
+import { onMounted } from 'vue'
+import { useState } from './store'
 
 export default {
   name: 'App',
@@ -19,11 +21,15 @@ export default {
     /* Set Notifier in API */
     const api = useApi()
     const notifier = useNotifier()
-
+    const state = useState();
     api && notifier && api.setNotifier(notifier)
 
     /* Don't include this in production! */
     import.meta.env.VITE_DEBUG === 'true' && test_api()
+
+    onMounted(() => {
+      state.access_token.value = localStorage.access_token;
+    })
   }
 }
 </script>
