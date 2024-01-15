124cb74d78af1b7e17fea4994629074d907ae6f2
mayar osama <mayarosama18@gmail.com>
2024-01-15 13:43:06 +0200

    calender

---
+++ client/src/components/calender.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import type { CalendarOptions } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction"; // Add this line
+import timeGridPlugin from "@fullcalendar/timegrid";
+import FullCalendar from "@fullcalendar/vue3";
+import { ref } from "vue";
+
+export default {
+  name: "calenderCshr",
+  components: {
+    FullCalendar,
+  },
+
+  setup() {
+    const vacationInfoHeaders = ["Annuals", "Emergencies", "Excuses"];
+    const options = ref<CalendarOptions>({
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin], // Include interactionPlugin
+      initialView: "dayGridMonth",
+    });
+    return {
+      vacationInfoHeaders,
+      options,
+    };
+  },
+};
+</script>
