aa543ead07a2b94e78bcf4de31abb085311f44de
MohamedElmdary <engm5081@gmail.com>
2024-01-21 17:24:11 +0200

    refactor: Update calender code

--- client/src/components/calender.vue
+++ client/src/components/calender.vue
@@ -1,343 +1,247 @@
 <template>
-  {{ selected }}
-  <v-checkbox v-model="selected" label="Meetings" value="Meetings"></v-checkbox>
-  <v-checkbox v-model="selected" label="Events" value="Events"></v-checkbox>
-  <v-checkbox v-model="selected" label="Vacations" value="Vacations"></v-checkbox>
+  <v-checkbox v-model="selected.meetings" label="Meetings" />
+  <v-checkbox v-model="selected.events" label="Events" />
+  <v-checkbox v-model="selected.vacations" label="Vacations" />
   <div class="ma-7 pa-7">
-    <FullCalendar :options="options" />
+    <FullCalendar
+      :options="{
+        ...options,
+        events: eventsOption
+      }"
+    />
   </div>
 
-  <CustomVDialog v-if="dates?.startStr" :routeQuery="dates?.startStr" :modelValue="showDialog[dates?.startStr]">
+  <CustomVDialog
+    v-if="dates?.startStr"
+    :routeQuery="dates?.startStr"
+    :modelValue="showDialog[dates?.startStr]"
+  >
     <v-card>
       <calenderRequest :dates="dates" @close-dialog="closeDialog(dates?.startStr)" />
     </v-card>
   </CustomVDialog>
 
-
-  <CustomVDialog v-if="isViewRequest && isMeeting && meeting" :routeQuery="meeting.id"
-    :modelValue="showDialog[meeting.id]">
+  <!-- <CustomVDialog
+    v-if="isViewRequest && isMeeting && meeting"
+    :routeQuery="meeting.id"
+    :modelValue="showDialog[meeting.id]"
+  >
     <v-card>
       <meetingCard :meeting="meeting" @close-dialog="closeDialog(meeting.id)" />
     </v-card>
+  </CustomVDialog> -->
 
-  </CustomVDialog>
-
-  <CustomVDialog v-if="isViewRequest && isEvent && event" :routeQuery="event.name" :modelValue="showDialog[event.name]">
+  <VDialog
+    v-if="isViewRequest && isEvent && event"
+    :routeQuery="event.name"
+    :modelValue="showDialog[event.name]"
+  >
     <v-card>
       <eventCard :event="event" @close-dialog="closeDialog(event.name)" />
     </v-card>
-  </CustomVDialog>
+  </VDialog>
 
-  <CustomVDialog v-if="isViewRequest && isLeave && vacation" :routeQuery="vacation.id"
-    :modelValue="showDialog[vacation.id]">
+  <VDialog
+    v-if="isViewRequest && isLeave && vacation"
+    :routeQuery="vacation.id"
+    :modelValue="showDialog[vacation.id]"
+  >
     <v-card>
       <vacationCard :vacation="vacation" @close-dialog="closeDialog(vacation.id)" />
     </v-card>
-  </CustomVDialog>
+  </VDialog>
 </template>
 <script lang="ts">
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction"; // Add this line
-import timeGridPlugin from "@fullcalendar/timegrid";
-import FullCalendar from "@fullcalendar/vue3";
-import listPlugin from '@fullcalendar/list';
-import calenderRequest from "@/components/calenderRequest.vue";
-import CustomVDialog from '@/components/vuetify/CustomVDialog.vue';
-import meetingCard from '@/components/cards/meetingCard.vue';
-import eventCard from '@/components/cards/eventCard.vue';
-import vacationCard from '@/components/cards/vacationCard.vue';
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction' // Add this line
+import timeGridPlugin from '@fullcalendar/timegrid'
+import FullCalendar from '@fullcalendar/vue3'
+import listPlugin from '@fullcalendar/list'
+import calenderRequest from '@/components/calenderRequest.vue'
+import meetingCard from '@/components/cards/meetingCard.vue'
+import eventCard from '@/components/cards/eventCard.vue'
+import vacationCard from '@/components/cards/vacationCard.vue'
 
+import { ref, computed } from 'vue'
+import type { Api } from '@/types'
+import { useApi } from '@/hooks'
+import type { EventClickArg } from '@fullcalendar/core/index.js'
+import type { CalendarApi } from '@fullcalendar/core/index.js'
+import { useAsyncState } from '@vueuse/core'
 
-import { onMounted, ref, watch } from "vue";
-import type { Api } from "@/types";
-import { $api } from "@/clients";
-import type { EventClickArg } from "@fullcalendar/core/index.js";
-import type { CalendarApi } from "@fullcalendar/core/index.js";
+function handelDates(start: any, end: any): any {
+  const dates = {
+    start,
+    end,
+    add: true,
+    cut: null,
+    endStr: null as null | string | Date,
+    startStr: null as null | string | Date
+  }
+
+  const endDate = new Date(dates.end || '')
+  if (dates.cut) {
+    endDate.setDate(endDate.getDate() - 1)
+  } else if (dates.add) {
+    endDate.setDate(endDate.getDate() + 1)
+  }
+
+  dates.end = endDate
+  dates.start = new Date(dates.start)
+
+  const endStr = `${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}`
+  const startStr = `${dates.start.getFullYear()}-${
+    dates.start.getMonth() + 1
+  }-${dates.start.getDate()}`
+
+  dates.endStr = endStr
+  dates.startStr = startStr
+  return dates
+}
+
+function normalizeVacation(v: Api.Vacation): any {
+  const dates = handelDates(v.from_date, v.end_date)
+
+  return {
+    title: 'Vacation',
+    color: 'primary',
+    start: dates.start,
+    end: dates.end,
+    backgroundColor: 'primary',
+    id: v.id.toString(),
+    allDay: true
+  }
+}
+
+function normalizeEvent(e: Api.Inputs.Event): any {
+  const dates = handelDates(e.from_date, e.end_date)
+
+  return {
+    title: 'Event',
+    color: 'primary',
+    start: dates.start,
+    end: dates.end,
+    backgroundColor: 'primary',
+    id: e.name,
+    allDay: true
+  }
+}
+
+function normalizeMeeting(m: Api.Meetings): any {
+  const dates = handelDates(m.date, m.date)
+
+  return {
+    title: 'Meeting',
+    color: 'secondary',
+    start: dates.start,
+    end: dates.end,
+    backgroundColor: 'primary',
+    id: m.id,
+    allDay: true
+  }
+}
 
 export default {
-  name: "calenderCshr",
+  name: 'calenderCshr',
   components: {
     FullCalendar,
-    CustomVDialog,
     calenderRequest,
-    meetingCard,
+    // meetingCard,
     eventCard,
-    vacationCard,
+    vacationCard
   },
 
-
   setup() {
-    const meetings = ref<Api.Meetings[]>([]);
-    const meeting = ref<Api.Meetings>();
-    const isViewRequest = ref<Boolean>(false);
-    const isEvent = ref<Boolean>(false);
-    const isMeeting = ref<Boolean>(false);
-    const isLeave = ref<Boolean>(false);
-    const events = ref<Api.Inputs.Event[]>([]);
-    const event = ref<Api.Inputs.Event>();
+    const $api = useApi()
+    const meetings = useAsyncState($api.meeting.list(), [])
+    const events = useAsyncState($api.event.list(), [])
+    const vacations = useAsyncState($api.vacations.list(), [])
 
-    const vacations = ref<Api.Vacation[]>([]);
-    const vacation = ref<Api.Vacation>();
-    const calendar = ref<CalendarApi>();
+    const meeting = ref<Api.Meetings>()
+    const isViewRequest = ref<Boolean>(false)
+    const isEvent = ref<Boolean>(false)
+    const isMeeting = ref<Boolean>(false)
+    const isLeave = ref<Boolean>(false)
+    const event = ref<Api.Inputs.Event>()
 
+    const vacation = ref<Api.Vacation>()
+    const calendar = ref<CalendarApi>()
 
-    const dates = ref<any>();
-    const selected = ref(["Events", "Vacations", "Meetings"]);
-    const isEventsPushed = ref<Boolean>(false);
-    const isMeetingPushed = ref<Boolean>(false);
-    const isLeavePushed = ref<Boolean>(false);
+    const dates = ref<any>()
+    console.log()
+
+    const selected = ref({ events: true, vacations: true, meetings: true })
     const showDialog = ref<{ [key: string]: boolean }>({})
     const onSelect = async (arg: any) => {
-      calendar.value = arg.view.calendar;
+      calendar.value = arg.view.calendar
       dates.value = arg
 
-      openDialog(arg.startStr);
-
-
-    };
-
-    // const validateDate = (startDate: Date) => {
-    //   const today = new Date();
-    //   today.setHours(0, 0, 0, 0); // Set the time to midnight for accurate comparison
-
-    //   if (startDate < today) {
-    //     console.error("Start date cannot be in the past.");
-    //     return false;
-    //   }
-
-    //   return true;
-    // };
+      openDialog(arg.startStr)
+    }
 
     const onClick = async (arg: EventClickArg) => {
-      calendar.value = arg.view.calendar;
+      calendar.value = arg.view.calendar
 
-      if (arg.event.title === "Meeting") {
-
-        meeting.value = meetings.value.filter(meeting => meeting.id === Number(arg.event.id))[0];
-        isMeeting.value = true;
-        openDialog(meeting.value.id);
-
-      } else if (arg.event.title === "Event") {
-
-        event.value = events.value.filter(event => event.name === arg.event.id)[0];
-        isEvent.value = true;
-        openDialog(event.value.name);
+      if (arg.event.title === 'Meeting') {
+        meeting.value = meetings.state.value.filter(
+          (meeting) => meeting.id === Number(arg.event.id)
+        )[0]
+        isMeeting.value = true
+        openDialog(meeting.value.id)
+      } else if (arg.event.title === 'Event') {
+        event.value = events.state.value.filter((event) => event.name === arg.event.id)[0]
+        isEvent.value = true
+        openDialog(event.value.name)
+      } else if (arg.event.title === 'Vacation') {
+        vacation.value = vacations.state.value.filter(
+          (vacation) => vacation.id === Number(arg.event.id)
+        )[0]
+        isLeave.value = true
+        openDialog(vacation.value.id)
       }
-      else if (arg.event.title === "Vacation") {
+      isViewRequest.value = true
+    }
 
-        vacation.value = vacations.value.filter(vacation => vacation.id === Number(arg.event.id))[0];
-        isLeave.value = true;
-        openDialog(vacation.value.id);
+    const plugins = [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin] as any
+    const eventsOption = computed(() => {
+      return [
+        ...(selected.value.meetings ? meetings.state.value.map(normalizeMeeting) : []),
+        ...(selected.value.events ? events.state.value.map(normalizeEvent) : []),
+        ...(selected.value.vacations ? vacations.state.value.map(normalizeVacation) : [])
+      ]
+    })
 
-      }
-      isViewRequest.value = true;
-    };
-
-
-    const options = ref<any>({
-      plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    const options = {
+      plugins,
       dayMaxEvents: true,
       initialView: 'dayGridMonth',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
-        right: 'dayGridMonth,dayGridWeek,listDay',
+        right: 'dayGridMonth,dayGridWeek,listDay'
       },
       selectable: true,
       weekends: true,
       select: onSelect,
       eventClick: onClick,
-      events: [],
-      editable: false,
-
-    });
-
-
-    const removeVacations = () => {
-      options.value.events = options.value.events.filter((event: any) => {
-        return event.title !== "Vacation";
-      });
+      editable: false
     }
-    const removeMeetings = () => {
-      options.value.events = options.value.events.filter((event: any) => {
-        return event.title !== "Meeting";
-      });
-    }
-    const removeEvents = () => {
-      options.value.events = options.value.events.filter((event: any) => {
-        return event.title !== "Event";
-      });
-    }
-
-    watch(
-      () => selected.value,
-      (value, oldValue) => {
-        if (value !== oldValue) {
-          
-          if (!selected.value.includes("Vacations")) {
-            removeVacations();
-            isLeavePushed.value = false;
-          } else {
-            if (!isLeavePushed.value) {
-              for (const vacation of vacations.value) {
-                pushVacation(vacation);
-              }
-            }
-          }
-
-
-          if (!selected.value.includes("Meetings")) {
-            removeMeetings();
-            isMeetingPushed.value = false;
-          } else {
-            if (!isMeetingPushed.value) {
-              for (const meeting of meetings.value) {
-                pushMeeting(meeting);
-              }
-            }
-          }
-
-
-
-          if (!selected.value.includes("Events")) {
-            removeEvents();
-            isEventsPushed.value = false;
-          }
-          else {
-            if (!isEventsPushed.value) {
-              for (const event of events.value) {
-                pushEvent(event);
-              }
-            }
-          }
-        }
-
-
-      },
-    );
-
-    async function getMeetings() {
-      meetings.value = await $api.meeting.list();
-      for (const meeting of meetings.value) {
-        pushMeeting(meeting);
-      }
-    }
-    async function getEvents() {
-      events.value = await $api.event.list();
-      for (const event of events.value) {
-        pushEvent(event);
-      }
-    }
-    async function getVacations() {
-      vacations.value = await $api.vacations.list();
-      for (const vacation of vacations.value) {
-        pushVacation(vacation);
-      }
-    }
-
-
-    onMounted(async () => {
-      await getMeetings();
-      await getEvents();
-      await getVacations();
-    })
-
-
-
-    const handelDates = (dates: any): any => {
-      const endDate = new Date(dates.end || '');
-      if (dates.cut) {
-        endDate.setDate(endDate.getDate() - 1);
-      } else if (dates.add) {
-        endDate.setDate(endDate.getDate() + 1);
-      }
-
-      dates.end = endDate;
-      dates.start = new Date(dates.start);
-
-      const endStr = `${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}`;
-      const startStr = `${dates.start.getFullYear()}-${dates.start.getMonth() + 1}-${dates.start.getDate()}`;
-
-      dates.endStr = endStr;
-      dates.startStr = startStr;
-      return dates;
-    };
-
-
-
-    const pushVacation = (v: Api.Vacation) => {
-      const dates = handelDates({
-        end: v.end_date,
-        start: v.from_date,
-        add: true,
-      });
-
-      const vacation = {
-        title: "Vacation",
-        color: "primary",
-        start: dates.start,
-        end: dates.end,
-        backgroundColor: "primary",
-        id: v.id,
-        allDay: true,
-      };
-      options.value.events = [...(options.value.events as []), vacation];
-      isLeavePushed.value = true;
-    };
-    const pushEvent = (e: Api.Inputs.Event) => {
-      const dates = handelDates({
-        end: e.end_date,
-        start: e.from_date,
-        add: true,
-      });
-
-      const event = {
-        title: "Event",
-        color: "primary",
-        start: dates.start,
-        end: dates.end,
-        backgroundColor: "primary",
-        id: e.name,
-        allDay: true,
-      };
-      options.value.events = [...(options.value.events as []), event];
-      isEventsPushed.value = true;
-    };
-    const pushMeeting = (m: Api.Meetings) => {
-      const dates = handelDates({
-        end: m.date,
-        start: m.date,
-        add: true,
-      });
-
-      const meeting = {
-        title: "Meeting",
-        color: "secondary",
-        start: dates.start,
-        end: dates.end,
-        backgroundColor: "secondary",
-        id: m.id,
-        allDay: true,
-      };
-      options.value.events = [...(options.value.events as []), meeting];
-      isMeetingPushed.value = true;
-    };
-
-
-
 
     function closeDialog(id: string | number) {
-      isViewRequest.value, isEvent.value, isMeeting.value, isLeave.value = false;
-      event.value, meeting.value, dates.value = undefined;
+      isViewRequest.value, isEvent.value, isMeeting.value, (isLeave.value = false)
+      event.value, meeting.value, (dates.value = undefined)
       showDialog.value[id] = false
     }
+
     async function openDialog(id: string | number) {
       showDialog.value[id] = true
     }
 
-
-
     return {
+      events,
+      meetings,
+      vacations,
+
       isViewRequest,
       isEvent,
       isLeave,
@@ -346,6 +250,7 @@ export default {
       event,
       showDialog,
       options,
+      eventsOption,
       dates,
       vacation,
       selected,
@@ -354,11 +259,10 @@ export default {
       onSelect,
       onClick,
       calenderRequest,
-      CustomVDialog,
       meetingCard,
       eventCard,
-      vacationCard,
-    };
-  },
-};
+      vacationCard
+    }
+  }
+}
 </script>
