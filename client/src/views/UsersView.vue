082135187bd3d778bdbd27f143f520a024f7570b
mayar osama <mayarosama18@gmail.com>
2024-01-23 00:17:23 +0200

    Using useAsyncState in UsersView

--- client/src/views/UsersView.vue
+++ client/src/views/UsersView.vue
@@ -4,9 +4,9 @@
 
     <v-row>
 
-
       <v-col class="d-flex flex-wrap justify-center">
-        <v-col v-for="user of users" :key="user?.id" xl="4" lg="6" md="12" sm="12" cols="12" class="px-5 py-5">
+        <v-col v-for="user of users.state.value" :key="user?.id" xl="4" lg="6" md="12" sm="12" cols="12"
+          class="px-5 py-5">
           <div class="mt-1 text-center py-0 w-100 ">
             <router-link class="routerLink" :to="{ name: 'profile', query: { id: user.id } }">
               <UserCard :user="user" />
@@ -27,6 +27,7 @@ import { $api } from '@/clients';
 import UserCard from "@/components/userCard.vue"
 import officeFilters from '@/components/filters.vue';
 import { useRoute } from 'vue-router';
+import { useAsyncState } from '@vueuse/core';
 export default {
   name: "UsersView",
   components: {
@@ -34,57 +35,51 @@ export default {
     officeFilters,
   },
   setup() {
-    const users = ref<Api.User[]>([]);
-    const countriesSet = new Set<string>();
     const countries = ref<Country[]>([]);
-    const countryMap = new Map<number, string>();
-
     const $route = useRoute()
+    let isFirstLoad = true;
 
 
-
-
-
-    async function getUsers() {
-      users.value = await $api.users.list({ "location_id": $route.query.location_id });
-      for (const user of users.value) {
-        countryMap.set(user.location.id, user.location.country);
-        countriesSet.add(user.location.country)
+    useAsyncState($api.users.list({ "location_id": $route.query.location_id }), [], {
+      onSuccess(data) {
+        users.execute(undefined, data)
       }
-      countries.value = Array.from(countryMap).map(([id, country]) => ({
-        id,
-        country,
-      }));
-    }
-    function setCountries() {
-      for (const user of users.value) {
-        countryMap.set(user.location.id, user.location.country);
-        countriesSet.add(user.location.country)
-      }
-      countries.value = Array.from(countryMap).map(([id, country]) => ({
-        id,
-        country,
-      }));
-    }
+    })
+    const users = useAsyncState(
+      async (users: Api.User[]) => {
+        if (isFirstLoad) {
+          countries.value = Array.from(new Set(users.map(user => user.location.id)))
+            .map(id => {
+              const user = users.find(u => u.location.id === id);
+              return { id: user?.location.id ?? 0, country: user?.location.country ?? '' };
+            });
+        }
+
+        isFirstLoad = false;
+        return users
+      },
+      [],
+      { immediate: false }
+    )
 
     watch(
       () => $route.query.location_id,
       async newValue => {
-        await getUsers();
+        useAsyncState($api.users.list({ "location_id": $route.query.location_id }), [], {
+          onSuccess(data) {
+            users.execute(undefined, data)
+          }
+        })
       },
     );
 
-    onMounted(async () => {
-      await getUsers();
-      setCountries();
 
-    })
 
     return {
       users,
+      countries,
       UserCard,
       officeFilters,
-      countries,
 
     }
   }
