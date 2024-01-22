522661443a2de2be83a364dc8867a6f36987918d
mayar osama <mayarosama18@gmail.com>
2024-01-22 15:51:08 +0200

    fixing typos

--- client/src/components/filters.vue
+++ client/src/components/filters.vue
@@ -3,8 +3,7 @@
     <v-card class="ma-5 pa-2 border rounded ma-2 align-self-start">
       <v-row class="ma-5">
         <v-col cols="12" class="py-5 px-2">
-          <span>You can change the selected office to discover the team in other offices, current elected office is your
-            office.
+          <span>You can change the selected office to discover the team in other offices.
           </span>
         </v-col>
       </v-row>
@@ -20,10 +19,8 @@
 
 
 
-            <v-autocomplete v-model="country" :items="countries" label="Country" @update:model-value="handleCountryChange"
+            <v-autocomplete v-model="country" :items="countries" label="Office" @update:model-value="handleCountryChange"
               return-object item-title="country">
-
-
             </v-autocomplete>
 
           </div>
