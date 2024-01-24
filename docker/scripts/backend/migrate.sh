5573af86565f6c7d5bcf9fa1dd91332d9aa778f8
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-01-24 13:18:17 +0200

    Run the migrations only on development mode.

--- docker/scripts/backend/migrate.sh
+++ docker/scripts/backend/migrate.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-if [ "$ENV" = "production" ]
-then
-    echo "Runing on production mode."
-    echo "Waiting for postgres to be running on: $DATABASE_HOST"
+if [ "$ENV" = "production" ]; then
+    echo "Running in production mode."
+    echo "Waiting for PostgreSQL to be running on: $DATABASE_HOST"
 
     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-      sleep 0.1
+        sleep 0.1
     done
 
     echo "PostgreSQL started"
+else
+    echo "Running in development mode."
+    cd server_dir
+    poetry run python3 manage.py flush --no-input
+    poetry run python3 manage.py migrate
+    poetry run python3 manage.py makemigrations
 fi
 
-cd server_dir
-poetry run python3 manage.py flush --no-input
-poetry run python3 manage.py migrate
-
+# Continue with the provided command or entry point
 exec "$@"
