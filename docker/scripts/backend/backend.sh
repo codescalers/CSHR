e242419f4eb58d2bed102da9126ba0d8d7b4acb1
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-01-24 13:38:05 +0200

    Update the backend.

--- docker/scripts/backend/backend.sh
+++ docker/scripts/backend/backend.sh
@@ -13,8 +13,12 @@ else
     echo "Running in development mode."
     cd server_dir
     poetry run python3 manage.py flush --no-input
-    poetry run python3 manage.py migrate
+    echo "Running the migration"
     poetry run python3 manage.py makemigrations
+    echo "Running the migrate"
+    poetry run python3 manage.py migrate
+    echo "Creating a superuser email: $DJANGO_SUPERUSER_EMAIL"
+    poetry run python3 manage.py createsuperuser --noinput
 fi
 
 # Continue with the provided command or entry point
