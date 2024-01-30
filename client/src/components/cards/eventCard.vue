6c4a23283880fb72f817bb5286eea380a6711494
ehab-hassan <hassane@incubaid.com>
2024-01-30 17:18:42 +0200

    update event card

--- client/src/components/cards/eventCard.vue
+++ client/src/components/cards/eventCard.vue
@@ -18,27 +18,21 @@
     </div>
     <!-- <v-card elevation="0" variant="outlined" color="white" class="px-4"> -->
     <v-row class="text-center mx-2 mt-2">
-      <v-col cols="3" class="mt-2 border" v-for="header in Headers" :key="header">
+      <v-col cols="6" class="mt-2 border" v-for="header in Headers" :key="header">
         {{ header }}
       </v-col>
     </v-row>
 
     <v-row class="text-center mx-2 mb-2">
-      <v-col cols="3" class="border">
-        {{ fromDate }}
+      <v-col cols="6" class="border">
+        {{ fromDate }}{{ fromTime }}
       </v-col>
 
-      <v-col cols="3" class="border">
-        {{ toDate }}
+      <v-col cols="6" class="border">
+        {{ toDate }}{{ toTime }}
       </v-col>
 
-      <v-col cols="3" class="border">
-        {{ fromTime }}
-      </v-col>
-
-      <v-col cols="3" class="border">
-        {{ toTime }}
-      </v-col>
+      
     </v-row>
   </v-container>
 
@@ -57,7 +51,7 @@ export default {
 
   setup(props) {
     const fromDateTime = new Date(props.event.from_date)
-    const Headers = ref(['From Date', 'To Date', 'From Time', 'To Time'])
+    const Headers = ref(['From Date', 'To Date'])
 
     const fromDate = ref<string>(fromDateTime.toISOString().split('T')[0])
     const fromTime = ref<string>(fromDateTime.toISOString().split('T')[1].substring(0, 8))
