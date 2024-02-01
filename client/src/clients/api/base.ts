582e70a963710a24b7f014be2eccafb73a44c44b
mayar osama <mayarosama18@gmail.com>
2024-02-01 10:57:55 +0200

    removing console logs

--- client/src/clients/api/base.ts
+++ client/src/clients/api/base.ts
@@ -110,7 +110,6 @@ export abstract class ApiClientBase {
     }
 
     if (err) {
-      console.log("err",err)
       ApiClientBase.$notifier?.notify({
         type: 'error',
         description: options.normalizeError?.(err, res) ?? ApiClientBase.normalizeError(err) ?? err
