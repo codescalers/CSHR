0d0a6b24c72d0071e849e04efb19cb67b7655b70
Thunder <mahmmoud.hassanein@gmail.com>
2024-07-07 10:51:00 +0300

    feat: Display the reporting to info: (#439)
    - Show the reporting to info card if the requested user is Admin.
    - Show the reporting to info card if the requested user is the same as the logged in user.
    - Created new types for the 'Location' and 'Reporting to' on the 'User' interface.

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -2,12 +2,17 @@
   <div>
     <v-row class="ma-5">
       <v-col cols="12" sm="12" md="3" class="pa-2 rounded ma-2 align-self-start">
+        <v-alert class="mb-4" color="primary" variant="outlined">User info</v-alert>
         <UserCard :loading="user.isLoading" :user="user.state.value" />
+        <div v-if="couldSeeReportingTo">
+          <v-divider class="mt-4 mb-4"></v-divider>
+          <v-alert class="mb-4" color="primary" variant="outlined">Reporting to</v-alert>
+          <UserCard :loading="user.isLoading" :user="user.state.value.reporting_to[0]" />
+        </div>
       </v-col>
       <v-col cols="12" sm="12" md="8" class="pa-2 border rounded position-relative ma-2">
         <div>
           <personalInformation :user="user.state.value" />
-          <!-- v-if="showBalance" -->
           <vacationBalance :balance="balance.state.value" />
         </div>
       </v-col>
@@ -25,6 +30,7 @@ import { useAsyncState } from '@vueuse/core'
 import profileImage from '@/components/profileImage.vue'
 import UserCard from '@/components/userCard.vue'
 import { ApiClientBase } from '@/clients/api/base'
+import type { Api } from '@/types'
 
 export default defineComponent({
   components: {
@@ -35,16 +41,25 @@ export default defineComponent({
 
   setup() {
     const $route = useRoute()
-    const storedUser = ApiClientBase.user
-    const user = useAsyncState(
+    const requestedUser = ApiClientBase.user.value?.fullUser
+
+    const user = useAsyncState<Api.User>(
       $route.query.id ? $api.users.getuser(Number($route.query.id)) : $api.myprofile.getUser(),
-      [],
+      {} as unknown as Api.User,
       {
         onSuccess(data) {
           balance.execute(undefined, data.id)
         }
       }
     )
+
+    const couldSeeReportingTo = computed(() => {
+      return (
+        user.state.value.reporting_to && user.state.value.reporting_to.length &&
+        ((requestedUser && user.state.value.id === requestedUser.id) ||
+          (requestedUser && requestedUser.user_type === 'Admin'))
+      )
+    })
 
     const balance = useAsyncState(
       async (id: number) => {
@@ -80,6 +95,7 @@ export default defineComponent({
     return {
       user,
       balance,
+      couldSeeReportingTo,
       // showBalance,
       vacationBalance,
       personalInformation,
