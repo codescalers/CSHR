50996158ca910f9b5384649b76a80402a798e827
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-07-09 13:02:27 +0300

    style: Updated the style of the Dashboard:
    - Set the active tab as a query param to load the selected tab on mount if there.
    - Added an active class on the selected tab to be clear for admins which tab is selected.

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -3,6 +3,7 @@ import moment from 'moment'
 import type { Api } from "@/types"
 import type { JWTokenObject } from "@/types"
 import type { ApiClient } from '@/clients/api'
+import { ref } from 'vue'
 
 export async function resolve<T>(promise: Promise<T>): Promise<[T, any]> {
   try {
@@ -21,14 +22,14 @@ export function devLog(...args: any[]): void {
   import.meta.env.DEV && args.length > 0 && console.log(...args)
 }
 
-export const DASHBOARD_ITEMS = [
-  { id: 1, name: 'Set Vacations' },
-  { id: 2, name: 'Set User Vacations' },
+export const DASHBOARD_ITEMS = ref([
+  { id: 1, name: 'Set Vacations', active: true, },
+  { id: 2, name: 'Set User Vacations', active: false, },
   // { id: 3, name: 'Update Office Vacations' },
-  { id: 4, name: 'Add Office' },
-  { id: 5, name: 'Add User' },
-  { id: 6, name: 'Update User Profile' }
-]
+  { id: 4, name: 'Add Office', active: false, },
+  { id: 5, name: 'Add User', active: false, },
+  { id: 6, name: 'Update User Profile', active: false, }
+])
 
 export const formatDate = (date: any) => moment(date).format('YYYY-MM-DD')
 
