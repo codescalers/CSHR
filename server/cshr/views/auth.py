b94a337ba5656dea870dceb0e58e472d21db767b
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-07-10 14:58:19 +0300

    feat: Updated the Auth endpoint:
    - A new object called 'calculate_balance' is ready now to be sent as payload to the auth endpoint to create user balance in the creation process.
    - The type of the 'calculate_balance' is bool, which indicates whether we'll create a new balance object for the user or not.
    - Created a condition to check the object and communicated with the front end with the required types.

--- server/cshr/views/auth.py
+++ server/cshr/views/auth.py
@@ -1,3 +1,4 @@
+from typing import Dict, List
 from rest_framework.generics import GenericAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -13,27 +14,67 @@ from cshr.serializers.auth import (
     MyTokenRefreshSerializer,
 )
 from cshr.api.response import CustomResponse
-from cshr.services.users import get_user_by_email
+from cshr.services.users import get_or_create_user_balance, get_user_by_email
+from cshr.models.vacations import REASON_CHOICES
+from cshr.models.users import User
 
 
 class RegisterApiView(GenericAPIView):
-    """Class RegisterAPIView to register a new user into database"""
+    """
+    API view for registering a new user into the database.
+    """
 
     serializer_class = RegisterSerializer
     permission_classes = [IsAdmin]
 
     def post(self, request: Request) -> Response:
-        """Method to register a new user"""
-        serializer = self.get_serializer(data=request.data)
+        """
+        Handle POST request to register a new user.
+
+        Parameters:
+        request (Request): The request object containing user data.
+
+        Returns:
+        Response: Custom response indicating success or failure.
+        """
+        # Handle optional image field
         if request.data.get("image") == "":
             request.data["image"] = None
+
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(joining_at=request.data.get("joining_at"))
+            user: User = serializer.save(joining_at=request.data.get("joining_at"))
+            calculate_balance: bool = request.data.get("calculate_balance")
+            user_balance: Dict = request.data.get("user_balance")
+
+            if calculate_balance:
+                if not user_balance:
+                    return CustomResponse.bad_request(
+                        message="The `calculate_balance` option is enabled while there is no `user_balance` object sent."
+                    )
+
+                # Check for required fields in the user_balance object
+                required_balance_fields: List[str] = [
+                    REASON_CHOICES.ANNUAL_LEAVES,
+                    REASON_CHOICES.EMERGENCY_LEAVE,
+                    REASON_CHOICES.LEAVE_EXCUSES,
+                ]
+                for field in required_balance_fields:
+                    if not user_balance.get(field):
+                        return CustomResponse.bad_request(
+                            message=f"The {field} is missing in the `user_balance` object."
+                        )
+
+                # Create or update user balance
+                get_or_create_user_balance(user, user_balance)
+
             return CustomResponse.success(
                 data=serializer.data,
                 message="User created successfully",
                 status_code=201,
             )
+
+        # Handle invalid serializer data
         return CustomResponse.bad_request(
             error=serializer.errors, message="User creation failed"
         )
