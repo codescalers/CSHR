e94946bb187e55f29ef8cc0bcd15c5763e4f1325
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-08-29 17:15:08 +0300

    feat: Enhance the offices view

--- client/src/views/OfficesView.vue
+++ client/src/views/OfficesView.vue
@@ -4,23 +4,41 @@
       <h2 class="font-weight-medium my-3">ThreeFold Offices</h2>
       <v-divider></v-divider>
     </div>
-    
-    <v-row v-if="isLoading" class="d-flex justify-center align-center" style="height: 750px;">
+
+    <v-row v-if="isLoading" class="d-flex justify-center align-center" style="height: 750px">
       <v-progress-circular color="primary" indeterminate :size="47"></v-progress-circular>
     </v-row>
 
     <div class="data" v-else-if="state?.results.length">
       <v-row>
-        <v-col xl="4" lg="6" md="12" sm="12" cols="12" v-for="office in state.results" :key="office.name">
+        <v-col
+          xl="4"
+          lg="6"
+          md="12"
+          sm="12"
+          cols="12"
+          v-for="office in state.results"
+          :key="office.name"
+        >
           <v-card class="pa-4 hover-card border bg-graytitle">
             <v-card-title>
               <v-row>
                 <v-col cols="2">
-                  <v-img v-if="isCustomIcon(office)" alt="Office Country" :src="getCountryFlagSrc(office)" />
+                  <v-img
+                    v-if="isCustomIcon(office)"
+                    alt="Office Country"
+                    :src="getCountryFlagSrc(office)"
+                  />
                   <v-icon v-else size="50" color="primary">mdi-office-building</v-icon>
                 </v-col>
                 <v-col>
-                  <p style="margin-left: -15px;">{{ office.name.length > 30 ? capitalize(office.name).slice(0, 30) + '...' : capitalize(office.name)}}</p>
+                  <p style="margin-left: -15px">
+                    {{
+                      office.name.length > 30
+                        ? capitalize(office.name).slice(0, 30) + '...'
+                        : capitalize(office.name)
+                    }}
+                  </p>
                 </v-col>
               </v-row>
             </v-card-title>
@@ -41,13 +59,23 @@
           </v-card>
         </v-col>
       </v-row>
-      <v-pagination class="mt-4 mb-4" v-model="page" :length="count" rounded="circle"></v-pagination>
+      <v-pagination
+        class="mt-4 mb-4"
+        v-model="page"
+        :length="count"
+        rounded="circle"
+      ></v-pagination>
     </div>
-    
+
     <div v-else>
       <NotFoundComponent :page-not-found="false" message="Seems like there are no offices." />
     </div>
-    <OfficeHolidaysDialog :selected-office="selectedOffice!" @close-dialog="closeDialog"/>
+    <OfficeHolidaysDialog
+      v-if="showDialog"
+      :show-dialog="showDialog"
+      :selected-office="selectedOffice!"
+      @close-dialog="closeDialog"
+    />
   </v-container>
 </template>
 
@@ -57,7 +85,7 @@ import NotFoundComponent from '@/components/NotFoundComponent.vue'
 import OfficeHolidaysDialog from '@/components/dialogs/OfficeHolidaysDialog.vue'
 import type { Api } from '@/types'
 import { useAsyncState } from '@vueuse/core'
-import { byCountry } from "country-code-lookup"
+import { byCountry } from 'country-code-lookup'
 import { capitalize, computed, ref, watch } from 'vue'
 
 export default {
@@ -65,14 +93,10 @@ export default {
   components: { NotFoundComponent, OfficeHolidaysDialog },
   setup() {
     const selectedOffice = ref<Api.LocationType | null>(null)
-    const requestYears = ref(5)
-    const loading = ref(false)
-    const tab = ref(new Date().getFullYear())
-    const holidays = ref<Api.OfficeHolidayDates[]>([])
-    const isOfficeSelected = computed(() => !!selectedOffice.value)
+    const showDialog = ref(false)
     const page = ref(1)
     const count = computed(() => (state.value ? Math.ceil(state.value!.count / 12) : 0))
-    
+
     const getOfficeCountry = (office: Api.LocationType): string => {
       const country = byCountry(office.country)
       return country?.internet ?? office.country
@@ -83,79 +107,55 @@ export default {
       return countryCode !== 'ch'
         ? `https://www.worldatlas.com/r/w425/img/flag/${countryCode}-flag.jpg`
         : `https://www.worldatlas.com/r/w425/img/flag/${countryCode}-flag.png`
-      }
-      
-      const isCustomIcon = (office: Api.LocationType): boolean => getOfficeCountry(office) !== office.country
-      
-      const formatWeekend = (weekend: string): string => weekend.replace(':', ' and ')
-
-      const viewOffice = (office: Api.LocationType): void => {
-      selectedOffice.value = office
     }
-    
+
+    const isCustomIcon = (office: Api.LocationType): boolean =>
+      getOfficeCountry(office) !== office.country
+
+    const formatWeekend = (weekend: string): string => weekend.replace(':', ' and ')
+
+    const viewOffice = (office: Api.LocationType): void => {
+      selectedOffice.value = office
+      showDialog.value = true
+    }
+
     const closeDialog = (): void => {
       selectedOffice.value = null
-      tab.value = new Date().getFullYear();
+      showDialog.value = false
     }
-    
-    watch(selectedOffice, () => {
-      isOfficeSelected.value
-    })
-    
-    const getYears = (options: {years: number}): number[] => {
-      const thisYear = new Date().getFullYear()
-      return Array.from({ length: options.years }, (_, i) => thisYear - i)
-    }
-    
-    const { execute, state, isLoading, } = useAsyncState(() => {
-      return $api.office.list({page: page.value,});
-    }, undefined, { immediate: true })
+
+    const { execute, state, isLoading } = useAsyncState(
+      () => {
+        return $api.office.list({ page: page.value })
+      },
+      undefined,
+      { immediate: true }
+    )
 
     watch(page, () => {
-      console.log(page.value)
-      execute();
+      execute()
     })
-
-    watch([selectedOffice, tab,], async () => {
-      holidays.value = [];
-      if(selectedOffice.value){
-        const req = useAsyncState(
-          $api.office.officeHolidays({year: tab.value, office_id: selectedOffice.value.id}),
-          [],
-          { immediate: true }
-        )
-        await req.execute()
-        holidays.value = req.state.value
-        loading.value = isLoading.value
-      }
-    })
-
-
 
     return {
       state,
       isLoading,
       selectedOffice,
-      isOfficeSelected,
-      tab,
       page,
       count,
-      requestYears,
-      holidays,
+      showDialog,
 
-      getYears,
       getCountryFlagSrc,
       isCustomIcon,
       formatWeekend,
       viewOffice,
       closeDialog,
-      capitalize,
+      capitalize
     }
   }
 }
 </script>
 <style>
-.btn-capitalize{
+.btn-capitalize {
   text-transform: capitalize !important;
 }
 </style>
